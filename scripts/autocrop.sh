77e8db2b385897f8147a00f69c1868d51fc47e7d
Krystian Bacławski <krystian.baclawski@gmail.com>
2012-01-22 18:27:49 -0800

    Add script that removes blank border around pic.

---
+++ scripts/autocrop.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#
+# This scripts removes extra "blank" pixels around the main part of image.
+#
+
+(( $# == 2 )) || exit 1
+
+declare -r input=`cd $(dirname "$1") && pwd`/`basename "$1"`
+declare -r output=`cd $(dirname "$2") && pwd`/`basename "$2"`
+
+[ ${input} != ${output} ] || exit 1
+
+echo "input: `identify $1`"
+
+convert ${input} -fuzz 10% -trim ${output}
+
+echo "output: `identify $2`"
