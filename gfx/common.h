3a92e415a9da0293a3a282205423da154bba29a5
Krystian Bacławski <krystian.baclawski@gmail.com>
2012-02-04 19:18:06 -0800

    Move common functions and datatypes to std.lib.

--- gfx/common.h
+++ gfx/common.h
@@ -1,21 +1,7 @@
 #ifndef __GFX2D_COMMON_H__
 #define __GFX2D_COMMON_H__
 
-#include <stdint.h>
-
-#ifdef FALSE
-#undef FALSE
-#endif
-
-#ifdef TRUE
-#undef TRUE
-#endif
-
-typedef enum { FALSE, TRUE } bool;
-
-#define abs(a) ((a) > 0 ? (a) : -(a))
-#define swapi(a,b)	{ (a)^=(b); (b)^=(a); (a)^=(b); }
-#define swapf(a,b)	{ float t; t = (b); (b) = (a); (a) = t; }
+#include "std/types.h"
 
 typedef struct Point {
   int16_t x, y;
