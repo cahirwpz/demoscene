4aa4b191223b98ad7d8930bd82f6ce93a3ee59e0
Krystian Bacławski <krystian.baclawski@gmail.com>
2012-02-04 21:43:40 -0800

    Better fun arg names. Log msgs if stack too short.

--- gfx/transformations.c
+++ gfx/transformations.c
@@ -2,8 +2,9 @@
 #include "gfx/transformations.h"
 #include "gfx/matrix2d.h"
 #include "gfx/matrix3d.h"
+#include "system/debug.h"
 
-static StackT *Matrices;
+static StackT *Matrices = NULL;
 
 bool TS_Init() {
   if (!Matrices)
@@ -25,15 +26,21 @@ void TS_Compose2D() {
   Matrix2D *a = (Matrix2D *)StackGet(Matrices, 2);
   Matrix2D *b = (Matrix2D *)StackGet(Matrices, 1);
 
-  if (a && b)
+  if (a && b) {
     M2D_Multiply((Matrix2D *)StackPush(Matrices), a, b);
+  } else {
+    LOG("TS_Compose2D: At least 2 matrices on stack required!\n");
+  }
 }
 
 void TS_Transpose2D() {
   Matrix2D *a = (Matrix2D *)StackGet(Matrices, 1);
 
-  if (a)
+  if (a) {
     M2D_Transpose((Matrix2D *)StackPush(Matrices), a);
+  } else {
+    LOG("TS_Transpose2D: At least 1 matrix on stack required!\n");
+  }
 }
 
 void TS_PushIdentity2D() {
@@ -44,12 +51,12 @@ void TS_PushRotation2D(float angle) {
   M2D_LoadRotation((Matrix2D *)StackPush(Matrices), angle);
 }
 
-void TS_PushScaling2D(float sx, float sy) {
-  M2D_LoadScaling((Matrix2D *)StackPush(Matrices), sx, sy);
+void TS_PushScaling2D(float scaleX, float scaleY) {
+  M2D_LoadScaling((Matrix2D *)StackPush(Matrices), scaleX, scaleY);
 }
 
-void TS_PushTranslation2D(float tx, float ty) {
-  M2D_LoadTranslation((Matrix2D *)StackPush(Matrices), tx, ty);
+void TS_PushTranslation2D(float moveX, float moveY) {
+  M2D_LoadTranslation((Matrix2D *)StackPush(Matrices), moveX, moveY);
 }
 
 Matrix2D *TS_GetMatrix2D(size_t num) {
