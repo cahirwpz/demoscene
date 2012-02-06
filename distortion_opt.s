f336c441d7cafd37660f5271d556496af9443b3f
Krystian Bacławski <krystian.baclawski@gmail.com>
2012-02-05 19:37:48 -0800

    PixBuf layout changes. Fix pointer dereferencing.

--- distortion_opt.s
+++ distortion_opt.s
@@ -29,9 +29,9 @@ _RenderDistortion
 
         ; prepare temporary pointers
         move.l  4(a3),a0
-        move.l  (a2),a2   ; CanvasT.pixbuf
-        move.l  4(a2),a1  ; PixBufT.data
-        move.l  4(a5),a5
+        move.l  (a2),a1   ; CanvasT.pixbuf
+        move.l  (a1),a1   ; PixBufT.data
+        move.l  (a5),a5
 
         ; clear offset registers
         clr.l   d2
