1e126840291c3870953a177484037ec10dbe434e
Krystian Bacławski <krystian.baclawski@gmail.com>
2012-02-05 19:38:35 -0800

    Add color number field to 8-bit image header.

--- scripts/img2raw.py
+++ scripts/img2raw.py
@@ -39,16 +39,17 @@ def main():
     if any(map(os.path.isfile, [rawFilePath, palFilePath])) and not args.force:
       raise SystemExit('Will not overwrite output files!')
 
-    with open(palFilePath, 'w') as palFile:
-      uniqueColors = len(image.getcolors())
+    uniqueColors = len(image.getcolors())
+    width, height = image.size
 
+    with open(palFilePath, 'w') as palFile:
       pal = array('B', image.getpalette()[:3*uniqueColors])
       palFile.write(struct.pack('>H', uniqueColors))
       palFile.write(pal.tostring())
 
     with open(rawFilePath, 'w') as rawFile:
       data = array('B', image.getdata())
-      rawFile.write(struct.pack('>HH', *image.size))
+      rawFile.write(struct.pack('>HHH', width, height, uniqueColors))
       rawFile.write(data.tostring())
   else:
     rawFilePath = '%s.24' % outputPath
