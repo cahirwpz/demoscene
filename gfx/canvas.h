46e9dcf7242b604e0c27ec1c614c5a40e6135e57
Krystian Bacławski <krystian.baclawski@gmail.com>
2012-02-08 22:31:44 -0800

    Foregroung color getter.

--- gfx/canvas.h
+++ gfx/canvas.h
@@ -17,6 +17,7 @@ void DeleteCanvas(CanvasT *canvas);
 
 void CanvasFill(CanvasT *canvas, uint8_t color);
 
+inline uint8_t GetCanvasFgCol(CanvasT *canvas) { return canvas->fg_col; }
 inline size_t GetCanvasWidth(CanvasT *canvas) { return canvas->pixbuf->width; }
 inline uint8_t *GetCanvasPixelData(CanvasT *canvas) { return canvas->pixbuf->data; }
 inline void CanvasPenMoveTo(CanvasT *canvas, int16_t x, int16_t y) { canvas->pen_pos.x = x; canvas->pen_pos.y = y; }
