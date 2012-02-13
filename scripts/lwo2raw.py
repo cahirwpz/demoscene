948cb53ed8f71a1dc402a0bfb793782f0231c2a4
Krystian Bacławski <krystian.baclawski@gmail.com>
2012-02-12 16:19:22 -0800

    Complete converter (dumps points and triangles).

--- scripts/lwo2raw.py
+++ scripts/lwo2raw.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 
+import argparse
 import logging
-import sys
+import os
 import struct
-import pprint
 
 from collections import namedtuple
 
@@ -40,8 +40,7 @@ class LWOB(iff.Parser):
     return chunks
 
   def handlePNTS(self, data):
-    return [Point(*struct.unpack('>fff', data[i:i+12]))
-            for i in xrange(0, len(data), 12)]
+    return [self.handlePoint(data[i:i+12]) for i in xrange(0, len(data), 12)]
 
   def handlePOLS(self, data):
     polygons = []
@@ -62,6 +61,7 @@ class LWOB(iff.Parser):
 
   def handleSURF(self, data):
     i = data.index('\0')
+
     name = data[:i]
     data = data[i:].lstrip('\0')
 
@@ -85,14 +85,65 @@ class LWOB(iff.Parser):
   def handleTWRP(self, data):
     return struct.unpack('>HH', data)
 
+  @property
+  def polygons(self):
+    for name, chunk in self._chunks:
+      if name == 'POLS':
+        return chunk
+        
+  @property
+  def points(self):
+    for name, chunk in self._chunks:
+      if name == 'PNTS':
+        return chunk
 
-def main(argv):
+
+def main():
+  parser = argparse.ArgumentParser(
+      description=('Converts Lightwave Object (LWOB) file to raw data.'))
+  parser.add_argument('-f', '--force', action='store_true',
+      help='If output object exists, the tool will overwrite it.')
+  parser.add_argument('input', metavar='LWOB', type=str,
+      help='Input LightWave object (LWOB) file name.')
+  parser.add_argument('output', metavar='RAWOBJ', type=str,
+      help='Output Raw Object file name.')
+  args = parser.parse_args()
+
+  args.input = os.path.abspath(args.input)
+
+  if not args.output.endswith('.robj'):
+    try:
+      name = args.input.rsplit('.', 1)[0]
+    except:
+      pass
+    args.output = os.path.join(name + '.robj')
+
+  if not os.path.isfile(args.input):
+    raise SystemExit('Input file "%s" does not exists!' % args.input)
+
+  if os.path.exists(args.output) and not args.force:
+    raise SystemExit('Raw Object file "%s" already exists (use'
+        ' "-f" to override).' % args.output)
+
   lwob = LWOB()
-  lwob.loadFile(sys.argv[1])
+  lwob.loadFile(args.input)
 
-  pprint.pprint(lwob._chunks)
+  logging.info('Object has %d points, and %d polygons.',
+      len(lwob.points), len(lwob.polygons))
+
+  with open(args.output, 'w') as robj:
+    robj.write(struct.pack('>HH', len(lwob.points), len(lwob.polygons)))
+
+    for x, y, z in lwob.points:
+      robj.write(struct.pack('>fff', x, y, z))
+
+    for points, _ in lwob.polygons:
+      assert len(points) == 3
+      robj.write(struct.pack('>HHH', *points))
+
+  logging.info('Wrote Raw Object file to: "%s"', args.output)
+
 
 if __name__ == '__main__':
-  FORMAT = '%(levelname)s: %(message)s'
-  logging.basicConfig(format=FORMAT, level=logging.DEBUG)
-  main(sys.argv)
+  logging.basicConfig(level=logging.DEBUG, format='%(levelname)s: %(message)s')
+  main()
