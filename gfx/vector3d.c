a2794b602fb27e9123929085f1359d8d189f96b0
Krystian Bacławski <krystian.baclawski@gmail.com>
2012-02-15 21:29:10 -0800

    Add 3D vector operations.

---
+++ gfx/vector3d.c
@@ -0,0 +1,44 @@
+#include <math.h>
+#include <string.h>
+
+#include "gfx/vector3d.h"
+
+void V3D_Add(Vector3D *d, Vector3D *a, Vector3D *b) {
+    d->x = a->x + b->x;
+    d->y = a->y + b->y;
+    d->z = a->z + b->z;
+}
+
+void V3D_Sub(Vector3D *d, Vector3D *a, Vector3D *b) {
+    d->x = a->x - b->x;
+    d->y = a->y - b->y;
+    d->z = a->z - b->z;
+}
+
+void V3D_Cross(Vector3D *d, Vector3D *a, Vector3D *b) {
+    Vector3D w;
+
+    w.x = a->y * b->z - b->y * a->z;
+    w.y = a->z * b->x - b->z * a->x;
+    w.z = a->x * b->y - b->x * a->y;
+
+    memcpy(d, &w, sizeof(w));
+}
+
+float V3D_Dot(Vector3D *a, Vector3D *b) {
+    return a->x * b->x + a->y * b->y + a->z * b->z;
+}
+
+float V3D_Length(Vector3D *a) {
+    return sqrt(a->x * a->x + a->y * a->y + a->z * a->z);
+}
+
+void V3D_Scale(Vector3D *d, Vector3D *a, float s) {
+    d->x = a->x * s;
+    d->y = a->y * s;
+    d->z = a->z * s;
+}
+
+void V3D_Normalize(Vector3D *d, Vector3D *a, float l) {
+    V3D_Scale(d, a, l / V3D_Length(a));
+}
---
+++ gfx/vector3d.h
@@ -0,0 +1,18 @@
+#ifndef __GFX_VECTOR3D_H__
+#define __GFX_VECTOR3D_H__
+
+#include "std/types.h"
+
+typedef struct {
+  float x, y, z, w;
+} Vector3D;
+
+void V3D_Add(Vector3D *d, Vector3D *a, Vector3D *b);
+void V3D_Sub(Vector3D *d, Vector3D *a, Vector3D *b);
+void V3D_Cross(Vector3D *d, Vector3D *a, Vector3D *b);
+float V3D_Dot(Vector3D *a, Vector3D *b);
+float V3D_Length(Vector3D *a);
+void V3D_Scale(Vector3D *d, Vector3D *a, float s);
+void V3D_Normalize(Vector3D *d, Vector3D *a, float l);
+
+#endif
