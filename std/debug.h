bda705ac1b6d980c029c8db7776488ba9dae54a8
Krystian Bacławski <krystian.baclawski@gmail.com>
2012-02-19 12:40:53 -0800

    Remove extraneous part of LOG prefix.

--- std/debug.h
+++ std/debug.h
@@ -6,7 +6,7 @@
 #define PANIC(...) _exit();
 #else
 #include <stdio.h>
-#define _LINE() printf("%s:%d:%s: ", __FILE__, __LINE__, __FUNCTION__)
+#define _LINE() printf("%s:%d: ", __FILE__, __LINE__)
 #define _EOL() putchar('\n')
 #define LOG(...) { _LINE(); printf(__VA_ARGS__); _EOL(); }
 #define PANIC(...) { LOG(__VA_ARGS__); _exit(); }
