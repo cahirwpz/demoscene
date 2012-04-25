ef18f7bdb88c700e189d31778d27b0cfa1d164e6
Krystian Bacławski <krystian.baclawski@gmail.com>
2012-04-25 15:55:56 -0700

    Shortly describe Queue implementation details.

--- std/queue.h
+++ std/queue.h
@@ -5,6 +5,12 @@
 
 typedef struct Queue QueueT;
 
+/*
+ * This is implementation of FIFO queue of limited size.  Space for all elements
+ * is preallocated. 
+ *
+ * Elements are pushed to / popped from the queue by copying their contents.
+ */
 QueueT *NewQueue(size_t size, size_t elemSize);
 
 void QueueReset(QueueT *self);
