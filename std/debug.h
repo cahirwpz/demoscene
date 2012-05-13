da555a0d31d9981f98f5cd4c0796ed5bc8f0b459
Krystian Bacławski <krystian.baclawski@gmail.com>
2012-05-13 12:06:50 +0200

    Add assert macro.

--- std/debug.h
+++ std/debug.h
@@ -4,12 +4,14 @@
 #ifdef NDEBUG
 #define LOG(...)
 #define PANIC(...) _exit();
+#define ASSERT(...)
 #else
 #include <stdio.h>
 #define _LINE() printf("%s:%d: ", __FILE__, __LINE__)
 #define _EOL() printf("\r\n")
 #define LOG(...) { _LINE(); printf(__VA_ARGS__); _EOL(); }
 #define PANIC(...) { LOG(__VA_ARGS__); _exit(); }
+#define ASSERT(cond, ...) { if (!(cond)) { LOG(__VA_ARGS__); _exit(); } }
 #endif
 
 #endif
