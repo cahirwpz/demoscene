526b46b04e52247275453fca4d3c8c94d45cb91b
Krystian Bacławski <krystian.baclawski@gmail.com>
2012-07-16 14:24:07 +0200

    Add stubs for fp 16.16 handling rountines.

---
+++ std/fp16.h
@@ -0,0 +1,13 @@
+#ifndef __STD_FP16_H__
+#define __STD_FP16_H__
+
+typedef struct {
+  int16_t integer;
+  uint16_t fraction;
+} Q16T;
+
+void CastFloatQ16(Q16T *result asm("a0"), float value asm("fp0"));
+void CastIntQ16(Q16T *result asm("a0"), int value asm("d0"));
+void IAddQ16(Q16T *result asm("a0"), Q16T value asm("d0"));
+
+#endif
