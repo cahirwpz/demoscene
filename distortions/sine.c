1d5895eafe7dae4a1b320f1240151b13a5e5262d
Krystian Bacławski <krystian.baclawski@gmail.com>
2012-07-18 15:48:04 +0200

    Corrected scaling coefficients.

--- distortions/sine.c
+++ distortions/sine.c
@@ -6,14 +6,16 @@ void GenerateSineDistortion(DistortionMapT *map,
                             size_t xFreq, float xAmp, float xShift,
                             size_t yFreq, float yAmp, float yShift)
 {
-  float *sinX = CalcSineTable(map->width, xFreq, xAmp, xShift);
-  float *sinY = CalcSineTable(map->height, yFreq, yAmp, yShift);
+  float *sinX = CalcSineTable(map->width,
+                              xFreq, xAmp * (int)map->textureW, xShift);
+  float *sinY = CalcSineTable(map->height,
+                              yFreq, yAmp * (int)map->textureH, yShift);
 
-  float dy = 1.0f / (int)map->height;
-  float dx = 1.0f / (int)map->width;
+  float dy = (float)map->textureH / (int)map->height;
+  float dx = (float)map->textureW / (int)map->width;
   float fx, fy;
-  size_t x, y;
-  int i;
+
+  size_t x, y, i;
 
   for (y = 0, i = 0, fy = 0.0f; y < map->height; y++, fy += dy)
     for (x = 0, fx = 0.0f; x < map->width; x++, fx += dx, i++)
