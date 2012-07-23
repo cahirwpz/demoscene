865bc14d61880bb10d62e132325614bd861f82cd
Krystian Bacławski <krystian.baclawski@gmail.com>
2012-07-23 17:00:24 +0200

    Distance calculating function.

--- gfx/vector3d.c
+++ gfx/vector3d.c
@@ -4,41 +4,47 @@
 #include "gfx/vector3d.h"
 
 void V3D_Add(Vector3D *d, Vector3D *a, Vector3D *b) {
-    d->x = a->x + b->x;
-    d->y = a->y + b->y;
-    d->z = a->z + b->z;
+  d->x = a->x + b->x;
+  d->y = a->y + b->y;
+  d->z = a->z + b->z;
 }
 
 void V3D_Sub(Vector3D *d, Vector3D *a, Vector3D *b) {
-    d->x = a->x - b->x;
-    d->y = a->y - b->y;
-    d->z = a->z - b->z;
+  d->x = a->x - b->x;
+  d->y = a->y - b->y;
+  d->z = a->z - b->z;
 }
 
 void V3D_Cross(Vector3D *d, Vector3D *a, Vector3D *b) {
-    Vector3D w;
+  Vector3D w;
 
-    w.x = a->y * b->z - b->y * a->z;
-    w.y = a->z * b->x - b->z * a->x;
-    w.z = a->x * b->y - b->x * a->y;
+  w.x = a->y * b->z - b->y * a->z;
+  w.y = a->z * b->x - b->z * a->x;
+  w.z = a->x * b->y - b->x * a->y;
 
-    memcpy(d, &w, sizeof(w));
+  memcpy(d, &w, sizeof(w));
 }
 
 float V3D_Dot(Vector3D *a, Vector3D *b) {
-    return a->x * b->x + a->y * b->y + a->z * b->z;
+  return a->x * b->x + a->y * b->y + a->z * b->z;
 }
 
 float V3D_Length(Vector3D *a) {
-    return sqrt(a->x * a->x + a->y * a->y + a->z * a->z);
+  return sqrt(a->x * a->x + a->y * a->y + a->z * a->z);
+}
+
+float V3D_Distance(Vector3D *a, Vector3D *b) {
+  Vector3D d;
+  V3D_Sub(&d, a, b);
+  return V3D_Length(&d);
 }
 
 void V3D_Scale(Vector3D *d, Vector3D *a, float s) {
-    d->x = a->x * s;
-    d->y = a->y * s;
-    d->z = a->z * s;
+  d->x = a->x * s;
+  d->y = a->y * s;
+  d->z = a->z * s;
 }
 
 void V3D_Normalize(Vector3D *d, Vector3D *a, float l) {
-    V3D_Scale(d, a, l / V3D_Length(a));
+  V3D_Scale(d, a, l / V3D_Length(a));
 }
--- gfx/vector3d.h
+++ gfx/vector3d.h
@@ -12,6 +12,7 @@ void V3D_Sub(Vector3D *d, Vector3D *a, Vector3D *b);
 void V3D_Cross(Vector3D *d, Vector3D *a, Vector3D *b);
 float V3D_Dot(Vector3D *a, Vector3D *b);
 float V3D_Length(Vector3D *a);
+float V3D_Distance(Vector3D *a, Vector3D *b);
 void V3D_Scale(Vector3D *d, Vector3D *a, float s);
 void V3D_Normalize(Vector3D *d, Vector3D *a, float l);
 
