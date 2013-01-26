0e51062ac27b8b7ba44bf6c56f15b44ad9b6d574
Krystian Bacławski <krystian.baclawski@gmail.com>
2013-01-26 23:07:19 +0100

    First attempt to implement subpixeling.

--- gfx/triangle.c
+++ gfx/triangle.c
@@ -22,43 +22,103 @@ static inline void remquo16(int32_t n, int16_t d, int16_t *quo, int16_t *rem) {
   *quo = n;
 }
 
+typedef int32_t fixed_t;
+
+static inline fixed_t float_to_fx(float v) {
+  return v * 16.0f;
+}
+
+static inline fixed_t fx_round(fixed_t v) {
+  return (v + 8) & ~15;
+}
+
+static inline fixed_t fx_ceil(fixed_t v) {
+  return (v + 15) & ~15;
+}
+
+static inline int fx_rint(fixed_t v) {
+  return (v + 8) >> 4;
+}
+
+static inline int fx_int(fixed_t v) {
+  return v >> 4;
+}
+
+static inline fixed_t fx_mul(fixed_t u, fixed_t v) {
+  return (u * v) >> 4;
+} 
+
 /* EdgeScan structure & routines. */
 
 typedef struct EdgeScan {
   int height, width;
 
   int16_t x, dx;
-  int16_t xerr, dxerr;
+  int16_t xerr, dxerr, nxerr;
   int16_t y;
 } EdgeScanT;
 
 static EdgeScanT l12, l13, l23;
 
-static inline void InitEdgeScan(EdgeScanT *e, int ys, int ye, int xs, int xe) {
-  int height = ye - ys;
-  int width = xe - xs;
+/*
+ * 1) DDA equation:
+ *
+ *       x_1 - x_0
+ *   x = --------- * (y - y_0) + x_0
+ *       y_1 - y_0
+ *
+ *   where x_0, x_1, y_0, y_1 are integers.
+ *
+ * 2) Ceil and floor equations:
+ *
+ *   a   | a |   a mod b
+ *   - = | - | + -------
+ *   b   + b +      b
+ *
+ *   + a +   | a - 1 |       | a + b - 1 |
+ *   | - | = | ----- | + 1 = | --------- |
+ *   | b |   +   b   +       +     b     +
+ */
 
-  /* Divisor, divident, remainder and quotient. */
-  e->height = height;
-  e->width = width;
+static void InitEdgeScan(EdgeScanT *e, fixed_t ys, fixed_t ye, fixed_t xs, fixed_t xe) {
+  fixed_t height = ye - ys;
+  fixed_t width = xe - xs;
+
+  e->height = fx_rint(ye) - fx_rint(ys);
+  e->width = fx_rint(xe) - fx_rint(xs);
+  e->nxerr = height;
 
   if (height)
     remquo16(width, height, &e->dx, &e->dxerr);
 
   /* X value and error. */
-  e->x = xs;
+  e->x = fx_rint(xs);
   e->xerr = 0;
 
-  /* Y value. */
-  e->y = ys;
+  {
+    fixed_t ys_centered = ys + float_to_fx(0.5f);
+    fixed_t ys_prestep = fx_ceil(ys_centered) - ys_centered;
+
+    if (height) {
+      int16_t r, q;
+
+      remquo16(fx_mul(width, ys_prestep), height, &q, &r);
+
+      e->x += q;
+      e->xerr += r;
+    }
+
+    /* Y value. */
+    e->y = fx_int(ys_centered);
+  }
 }
 
 static inline void IterEdgeScan(EdgeScanT *e) {
   e->x += e->dx;
   e->xerr += e->dxerr;
 
-  if (e->xerr >= e->height) {
-    e->xerr -= e->height;
+  if (e->xerr >= e->nxerr) {
+    e->xerr -= e->nxerr;
     e->x++;
   }
 
@@ -100,7 +160,7 @@ __regargs static void DrawTriangleSegment(EdgeScanT *left, EdgeScanT *right,
     uint8_t *pixels = segment.pixels + x;
     uint8_t color = segment.color;
 
-    LOG("Line: (%d, %f..%f)", left->y, left->x, right->x);
+    LOG("Line: (%d, %d..%d)", left->y, left->x, right->x);
 
     do {
       *pixels++ = color;
@@ -118,12 +178,12 @@ void DrawTriangle(CanvasT *canvas, float x1f, float y1f, float x2f, float y2f,
                   float x3f, float y3f)
 {
   {
-    int x1 = lroundf(x1f);
-    int y1 = lroundf(y1f);
-    int x2 = lroundf(x2f);
-    int y2 = lroundf(y2f);
-    int x3 = lroundf(x3f);
-    int y3 = lroundf(y3f);
+    fixed_t x1 = float_to_fx(x1f);
+    fixed_t y1 = float_to_fx(y1f);
+    fixed_t x2 = float_to_fx(x2f);
+    fixed_t y2 = float_to_fx(y2f);
+    fixed_t x3 = float_to_fx(x3f);
+    fixed_t y3 = float_to_fx(y3f);
 
     if (y1 > y2) {
       swapr(x1, x2);
@@ -140,7 +200,7 @@ void DrawTriangle(CanvasT *canvas, float x1f, float y1f, float x2f, float y2f,
       swapr(y2, y3);
     }
 
-    LOG("Triangle: (%d, %d) (%d, %d) (%d, %d).", x1, y1, x2, y2, x3, y3);
+    LOG("Triangle: (%f, %f) (%f, %f) (%f, %f).", x1f, y1f, x2f, y2f, x3f, y3f);
 
     InitEdgeScan(&l12, y1, y2, x1, x2);
     InitEdgeScan(&l13, y1, y3, x1, x3);
