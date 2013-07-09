385dd5b6e90a5f1278755d0e7395d9b69e2039ae
Krystian Bacławski <krystian.baclawski@gmail.com>
2013-07-09 15:26:16 +0200

    Rectangle drawing.

---
+++ gfx/rectangle.h
@@ -0,0 +1,9 @@
+#ifndef __GFX_RECTANGLE_H__
+#define __GFX_RECTANGLE_H__
+
+#include "gfx/pixbuf.h"
+
+void DrawRectange(PixBufT *canvas,
+                  int x, int y, unsigned int w, unsigned int h);
+
+#endif
