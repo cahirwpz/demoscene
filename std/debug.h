8792b700f0bc2f6582943348bbb6538c664f43c4
Krystian Bacławski <krystian.baclawski@gmail.com>
2013-08-30 14:20:09 +0200

    ASSERT and PANIC use exceptions now.

--- std/debug.h
+++ std/debug.h
@@ -1,17 +1,19 @@
 #ifndef __STD_DEBUG_H__
 #define __STD_DEBUG_H__
 
+#include "std/exception.h"
+
 #ifdef NDEBUG
 #define LOG(...)
-#define PANIC(...) _exit();
+#define PANIC(...) RAISE
 #define ASSERT(...)
 #else
 #include <stdio.h>
 #define _LINE() printf("%s:%d: ", __FILE__, __LINE__)
 #define _EOL() printf("\r\n")
 #define LOG(...) { _LINE(); printf(__VA_ARGS__); _EOL(); }
-#define PANIC(...) { LOG(__VA_ARGS__); _exit(); }
-#define ASSERT(cond, ...) { if (!(cond)) { LOG(__VA_ARGS__); _exit(); } }
+#define PANIC(...) { LOG(__VA_ARGS__); RAISE; }
+#define ASSERT(cond, ...) { if (!(cond)) { LOG(__VA_ARGS__); RAISE; } }
 #endif
 
 #endif
