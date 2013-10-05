fc5b51e2be48d8b7b1a694b2bfd3289988a1d567
Krystian Bacławski <krystian.baclawski@gmail.com>
2013-10-05 13:48:11 +0200

    From now on 68060 is required.

--- system/check.c
+++ system/check.c
@@ -9,14 +9,14 @@
 bool SystemCheck() {
   bool kickv40 = SysBase->LibNode.lib_Version >= 40;
   bool chipaga = GfxBase->ChipRevBits0 & (GFXF_AA_ALICE|GFXF_AA_LISA);
-  bool cpu68040 = SysBase->AttnFlags & AFF_68040;
+  bool cpu68060 = SysBase->AttnFlags & AFF_68060;
   bool fpu68882 = SysBase->AttnFlags & AFF_68882;
 
   Printf("System check:\r\n");
   Printf(" - Kickstart v40 : %s\r\n", (ULONG)(kickv40 ? "yes" : "no"));
   Printf(" - ChipSet AGA : %s\r\n", (ULONG)(chipaga ? "yes" : "no"));
-  Printf(" - CPU 68040 : %s\r\n", (ULONG)(cpu68040 ? "yes" : "no"));
+  Printf(" - CPU 68060 : %s\r\n", (ULONG)(cpu68060 ? "yes" : "no"));
   Printf(" - FPU 68882 : %s\r\n", (ULONG)(fpu68882 ? "yes" : "no"));
 
-  return (kickv40 && cpu68040 && fpu68882 && chipaga);
+  return (kickv40 && cpu68060 && fpu68882 && chipaga);
 }
