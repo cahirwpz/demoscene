889b92cfed7988fe0469737c3f15310d320d328e
Krystian Bacławski <krystian.baclawski@gmail.com>
2013-10-07 11:17:21 +0200

    Optimized version.

--- a500/blitter.c
+++ a500/blitter.c
@@ -4,34 +4,40 @@
 __regargs void BlitterLine(BitmapT *bitmap, UWORD b,
                            UWORD x1, UWORD y1, UWORD x2, UWORD y2)
 {
-  UWORD dx = (x2 > x1) ? (x2 - x1) : (x1 - x2);
-  UWORD dy = (y2 > y1) ? (y2 - y1) : (y1 - y2);
-  UWORD dmax = max(dx, dy);
-  UWORD dmin = min(dx, dy);
+  UBYTE *data = bitmap->planes[b];
   UWORD bwidth = bitmap->width / 8;
-  LONG bltapt = 4 * dmin - 2 * dmax;
+  WORD dx, dy, dmax, dmin, bltapt, begin;
 
-  custom->bltadat = 0x8000;
-  custom->bltbdat = 0xffff; /* Line texture pattern. */
-  custom->bltafwm = 0xffff;
-  custom->bltalwm = 0xffff;
+  dx = x2 - x1;
+  if (dx < 0)
+    dx = -dx;
 
-  custom->bltamod = 4 * (dmin - dmax);
-  custom->bltbmod = 4 * dmin;
+  dy = y2 - y1;
+  if (dy < 0)
+    dy = -dy;
+
+  if (dx > dy) {
+    dmax = dx;
+    dmin = dy;
+  } else {
+    dmax = dy;
+    dmin = dx;
+  }
+
+  bltapt = (dmin << 2) - (dmax << 1);
+  custom->bltapt = (APTR)(LONG)bltapt;
+
+  custom->bltamod = (dmin - dmax) << 2;
+  custom->bltbmod = dmin << 2;
   custom->bltcmod = bwidth;
   custom->bltdmod = bwidth;
 
-  custom->bltapt = (APTR)bltapt;
-
   /* Word containing the first pixel of the line. */
-  {
-    UBYTE *data = bitmap->planes[b];
+  begin = (UWORD)(bwidth * y1) + ((x1 >> 3) ^ 1);
+  data = &data[begin];
 
-    data += bwidth * y1 + ((x1 / 8) ^ 1);
-
-    custom->bltcpt = data;
-    custom->bltdpt = data;
-  }
+  custom->bltcpt = data;
+  custom->bltdpt = data;
 
   /* Minterm is either $ca or $4a */
   custom->bltcon0 =
@@ -82,5 +88,10 @@ __regargs void BlitterLine(BitmapT *bitmap, UWORD b,
     custom->bltcon1 = bltcon1;
   }
 
-  custom->bltsize = (dmax + 1) * 64 + 2;
+  custom->bltadat = 0x8000;
+  custom->bltbdat = -1; /* Line texture pattern. */
+  custom->bltafwm = -1;
+  custom->bltalwm = -1;
+
+  custom->bltsize = ((dmax + 1) << 6) + 2;
 }
