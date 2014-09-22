bf477e20f62d86cfe26568d760793e6399af92d7
Krystian Bacławski <krystian.baclawski@gmail.com>
2014-09-22 20:01:09 +0200

    Calculate intesified color.

--- a500/sys/color.h
+++ a500/sys/color.h
@@ -22,4 +22,23 @@ static inline UWORD ColorTransitionRGB(UBYTE sr, UBYTE sg, UBYTE sb, UBYTE dr, U
   return (colortab[r] << 8) | (colortab[g] << 4) | colortab[b];
 }
 
+static inline UWORD ColorIntensifyRGB(UBYTE dr, UBYTE dg, UBYTE db, UWORD step) {
+  WORD r = colortab[(dr & 0xf0) | step];
+  WORD g = colortab[(dg & 0xf0) | step];
+  WORD b = colortab[(db & 0xf0) | step];
+
+  r += (dr >> 4);
+  g += (dg >> 4);
+  b += (db >> 4);
+
+  if (r < 0) r = 0;
+  if (r > 15) r = 15;
+  if (g < 0) g = 0;
+  if (g > 15) g = 15;
+  if (b < 0) b = 0;
+  if (b > 15) b = 15;
+  
+  return (r << 8) | (g << 4) | b;
+}
+
 #endif
