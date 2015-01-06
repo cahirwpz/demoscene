705055fcef6ec30a315a17af0d43a08614e886a5
Krystian Bacławski <krystian.baclawski@gmail.com>
2015-01-06 10:32:02 +0100

    Optimize color transition routine.

--- a500/sys/color.h
+++ a500/sys/color.h
@@ -7,9 +7,9 @@ extern UBYTE colortab[4096];
 
 /* Each argument must be in range 0-15. */
 static inline UWORD ColorTransition(UWORD from, UWORD to, UWORD step) {
-  WORD r = (from & 0xf00) | ((to & 0xf00) >> 4) | step;
-  WORD g = ((from & 0x0f0) << 4) | (to & 0x0f0) | step;
-  WORD b = ((from & 0x00f) << 8) | ((to & 0x00f) << 4) | step;
+  WORD r = (from & 0xf00) | ((to >> 4) & 0x0f0) | step;
+  WORD g = ((from << 4) & 0xf00) | (to & 0x0f0) | step;
+  WORD b = ((from << 8) & 0xf00) | ((to << 4) & 0x0f0) | step;
   
   return (colortab[r] << 4) | colortab[g] | (colortab[b] >> 4);
 }
