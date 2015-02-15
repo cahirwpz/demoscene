b69affd29a92fff3017ec4be9052f95f8cbe870e
Krystian Bacławski <krystian.baclawski@gmail.com>
2015-02-15 10:03:24 +0100

    Enhancements to model blitter based chunky-to-planar.

--- scripts/c2p.py
+++ scripts/c2p.py
@@ -47,6 +47,15 @@ class Bit(object):
     else:
       return Bit('?', None)
 
+  def __invert__(self):
+    a = self._value
+    if a == 1:
+      return Bit(0, self._color)
+    elif a == 0:
+      return Bit(1, self._color)
+    else:
+      return Bit('?', None)
+
   def __str__(self):
     return "\033[%s%s%s" % (self._color, str(self._value).rjust(3), "\033[0m")
 
@@ -74,46 +83,34 @@ class Word(object):
   def __iter__(self):
     return iter(self._value)
 
-  def _index(self, i):
-    return (self._width - 1) - i
-
-  def _slice(self, s):
-    return slice(self._index(s.start), self._index(s.stop - 1))
-
   def __getitem__(self, index):
     if type(index) == slice:
-      s = self._slice(index)
-      return Word(self._value[s])
+      return Word(self._value[index])
     else:
-      i = self._index(index)
-      return self._value[i]
+      return self._value[index]
 
   def __setitem__(self, index, value):
-    if type(index) == slice:
-      s = self._slice(index)
-      self._value[s] = value
-    else:
-      i = self._index(index)
-      self._value[i] = value
+    self._value[index] = value
 
   def __len__(self):
     return self._width
 
-  def lsr(self, n):
-    w = Word(len(self))
-    w[(len(w) - 1) - n: 0] = self[len(w) - 1: n]
-    return w
+  def __rshift__(self, s):
+    n = len(self)
+    return Word(s) + self[:n - s]
 
-  def lsl(self, n):
-    w = Word(len(self))
-    w[(len(w) - 1):n] = self[(len(w) - 1 - n):0]
-    return w
+  def __lshift__(self, s):
+    return self[s:] + Word(s)
 
-  def ror(self, n):
-    w = Word(len(self))
-    w[(len(w) - 1) - n: 0] = self[len(w) - 1: n]
-    w[(len(w) - 1):(len(w) - n)] = self[n - 1:0]
-    return w
+  def __invert__(self):
+    return Word(~a for a in self)
+
+  def ror(self, s):
+    n = len(self)
+    return self[n - s:] + self[:n - s]
+
+  def rol(self, s):
+    return self[s:] + self[:s]
 
   def __or__(self, word):
     if len(self) != len(word):
@@ -134,11 +131,62 @@ class Word(object):
     return "".join(map(str, self))
 
 
+class Channel(object):
+  def __init__(self, data, start, modulo):
+    self.pos = start
+    self.data = data
+    self.modulo = modulo
+
+  def get(self):
+    n = self.data[self.pos]
+    self.pos += 1
+    return n
+
+  def put(self, n):
+    self.data[self.pos] = n
+    self.pos += 1
+
+  def next(self):
+    self.pos += self.modulo
+
+
+def MakeArray(MakeWord):
+  return [MakeWord("abcd", color="31m"),
+          MakeWord("efgh", color="32m"),
+          MakeWord("ijkl", color="33m"),
+          MakeWord("mnop", color="34m"),
+          MakeWord("qrst", color="35m"),
+          MakeWord("uvwx", color="36m"),
+          MakeWord("ABCD", color="31;1m"),
+          MakeWord("EFGH", color="32;1m"),
+          MakeWord("IJKL", color="33;1m"),
+          MakeWord("MNOP", color="34;1m"),
+          MakeWord("QRST", color="35;1m"),
+          MakeWord("UVWX", color="36;1m")]
+
+
+def FreshArray(size, bits):
+  return [Word(bits) for i in range(size)]
+
+
+def FreshBitplanes(planes, size, bits):
+  return [[Word(bits) for i in range(size)] for j in range(planes)]
+
+
+def Blit(fn, height, width, A, B, C):
+  for i in range(height):
+    for j in range(width):
+      C.put(fn(A.get(), B.get()))
+    A.next()
+    B.next()
+    C.next()
+
+
 def c2p_2_1_4():
-  m0 = Word.Mask('33333333')
-  m1 = Word.Mask('cccccccc')
-  m2 = Word.Mask('55555555')
-  m3 = Word.Mask('aaaaaaaa')
+  m0 = Word.Mask('3333')
+  m1 = Word.Mask('cccc')
+  m2 = Word.Mask('5555')
+  m3 = Word.Mask('aaaa')
 
   print "c2p 2x1 4-bitplanes".center(96, '-')
 
@@ -165,12 +213,12 @@ def c2p_2_1_4():
 
 
 def c2p_1_1_8():
-  m0 = Word.Mask('0f0f0f0f')
-  m1 = Word.Mask('f0f0f0f0')
-  m2 = Word.Mask('33333333')
-  m3 = Word.Mask('cccccccc')
-  m4 = Word.Mask('55555555')
-  m5 = Word.Mask('aaaaaaaa')
+  m0 = Word.Mask('0f0f')
+  m1 = Word.Mask('f0f0')
+  m2 = Word.Mask('3333')
+  m3 = Word.Mask('cccc')
+  m4 = Word.Mask('5555')
+  m5 = Word.Mask('aaaa')
 
   print "c2p 1x1 8-bitplanes".center(96, '-')
 
@@ -218,17 +266,14 @@ def c2p_1_1_8():
 
   printall("Swap 1x1:", d0, d1, d2, d3, d4, d5, d6, d7)
 
-def c2p_1_1_4_accurate():
-  m0 = Word.Mask('00ff')
-  m1 = Word.Mask('ff00')
-  m2 = Word.Mask('0f0f')
-  m3 = Word.Mask('f0f0')
-  m4 = Word.Mask('3333')
-  m5 = Word.Mask('cccc')
-  m6 = Word.Mask('5555')
-  m7 = Word.Mask('aaaa')
 
-  print "=[ c2p 1x1 4bpl (accurate) ]=".center(48, '-')
+def c2p_1_1_4_blitter(bitplane_output=True):
+  m0 = Word.Mask('00ff')
+  m1 = Word.Mask('0f0f')
+  m2 = Word.Mask('3333')
+  m3 = Word.Mask('5555')
+
+  print "=[ c2p 1x1 4bpl (blitter) ]=".center(48, '-')
 
   def MakeWord(chars, color):
     bits = []
@@ -237,97 +282,120 @@ def c2p_1_1_4_accurate():
         bits.append(Bit.Var(c, i, color))
     return Word(bits)
 
-  l0 = MakeWord("abcd", color="31m")
-  l1 = MakeWord("efgh", color="32m")
-  l2 = MakeWord("ijkl", color="33m")
-  l3 = MakeWord("mnop", color="34m")
+  A = MakeArray(MakeWord)
+  N = len(A)
+  printall("Data:", *A)
 
-  printall("Data:", l0, l1, l2, l3)
+  B = FreshArray(N, 16)
+  Blit(lambda a, b: ((a >> 8) & m0) | (b & ~m0),
+       N / 4, 2, Channel(A, 2, 2), Channel(A, 0, 2), Channel(B, 0, 2))
+  Blit(lambda a, b: ((a << 8) & ~m0) | (b & m0),
+       N / 4, 2, Channel(A, 0, 2), Channel(A, 2, 2), Channel(B, 2, 2))
+  printall("Swap 8x4:", *B)
 
-  a0 = (l0 & m1) | (l2.lsr(8) & m0)
-  a1 = (l1 & m1) | (l3.lsr(8) & m0)
-  a2 = (l2 & m0) | (l0.lsl(8) & m1)
-  a3 = (l3 & m0) | (l1.lsl(8) & m1)
+  C = FreshArray(N, 16)
+  Blit(lambda a, b: ((a >> 4) & m1) | (b & ~m1),
+       N / 2, 1, Channel(B, 1, 1), Channel(B, 0, 1), Channel(C, 0, 1))
+  Blit(lambda a, b: ((a << 4) & ~m1) | (b & m1),
+       N / 2, 1, Channel(B, 0, 1), Channel(B, 1, 1), Channel(C, 1, 1))
+  printall("Swap 4x2:", *C)
 
-  printall("Swap 8x4:", a0, a1, a2, a3)
+  D = FreshArray(N, 16)
+  Blit(lambda a, b: ((a >> 2) & m2) | (b & ~m2),
+       N / 4, 2, Channel(C, 2, 2), Channel(C, 0, 2), Channel(D, 0, 2))
+  Blit(lambda a, b: ((a << 2) & ~m2) | (b & m2),
+       N / 4, 2, Channel(C, 0, 2), Channel(C, 2, 2), Channel(D, 2, 2))
+  printall("Swap 2x2:", *D)
 
-  b0 = (a0 & m3) | (a1.lsr(4) & m2)
-  b1 = (a1 & m2) | (a0.lsl(4) & m3)
-  b2 = (a2 & m3) | (a3.lsr(4) & m2)
-  b3 = (a3 & m2) | (a2.lsl(4) & m3)
+  if bitplane_output:
+    E = FreshBitplanes(4, N / 4, 16)
+    Blit(lambda a, b: ((a >> 1) & m3) | (b & ~m3),
+         N / 4, 1, Channel(D, 1, 3), Channel(D, 0, 3), Channel(E[0], 0, 0))
+    Blit(lambda a, b: ((a >> 1) & m3) | (b & ~m3),
+         N / 4, 1, Channel(D, 3, 3), Channel(D, 2, 3), Channel(E[2], 0, 0))
+    Blit(lambda a, b: ((a << 1) & ~m3) | (b & m3),
+         N / 4, 1, Channel(D, 0, 3), Channel(D, 1, 3), Channel(E[1], 0, 0))
+    Blit(lambda a, b: ((a << 1) & ~m3) | (b & m3),
+         N / 4, 1, Channel(D, 2, 3), Channel(D, 3, 3), Channel(E[3], 0, 0))
+    print("Bitplanes:")
+    printall("[0]:", *E[0])
+    printall("[1]:", *E[1])
+    printall("[2]:", *E[2])
+    printall("[3]:", *E[3])
+  else:
+    E = FreshArray(N, 16)
+    Blit(lambda a, b: ((a >> 1) & m3) | (b & ~m3),
+         N / 2, 1, Channel(D, 1, 1), Channel(D, 0, 1), Channel(E, 0, 1))
+    Blit(lambda a, b: ((a << 1) & ~m3) | (b & m3),
+         N / 2, 1, Channel(D, 0, 1), Channel(D, 1, 1), Channel(E, 1, 1))
+    printall("Swap 1x1:", *E)
 
-  printall("Swap 4x2:", b0, b1, b2, b3)
 
-  c0 = (b0 & m5) | (b2.lsr(2) & m4)
-  c1 = (b1 & m5) | (b3.lsr(2) & m4)
-  c2 = (b2 & m4) | (b0.lsl(2) & m5)
-  c3 = (b3 & m4) | (b1.lsl(2) & m5)
-
-  printall("Swap 2x2:", c0, c1, c2, c3)
-
-  d0 = (c0 & m7) | (c1.lsr(1) & m6)
-  d1 = (c1 & m6) | (c0.lsl(1) & m7)
-  d2 = (c2 & m7) | (c3.lsr(1) & m6)
-  d3 = (c3 & m6) | (c2.lsl(1) & m7)
-
-  printall("Swap 1x1:", d0, d1, d2, d3)
-
-
-def c2p_1_1_4_mangled():
+def c2p_1_1_4_blitter_mangled(bitplane_output=True):
   m0 = Word.Mask('00ff')
-  m1 = Word.Mask('ff00')
-  m2 = Word.Mask('0f0f')
-  m3 = Word.Mask('f0f0')
-  m4 = Word.Mask('3333')
-  m5 = Word.Mask('cccc')
+  m1 = Word.Mask('0f0f')
+  m2 = Word.Mask('3333')
 
-  print "=[ c2p 1x1 4bpl (mangled) ]=".center(48, '-')
+  print "=[ c2p 1x1 4bpl (blitter mangled) ]=".center(48, '-')
 
   def MakeWord(c, color):
     return Word([Bit.Var(c[0], 0, color),
                  Bit.Var(c[1], 0, color),
-                 Bit.Var(c[0], 1, color),
-                 Bit.Var(c[1], 1, color),
                  Bit.Var(c[0], 2, color),
                  Bit.Var(c[1], 2, color),
+                 Bit.Var(c[0], 1, color),
+                 Bit.Var(c[1], 1, color),
                  Bit.Var(c[0], 3, color),
                  Bit.Var(c[1], 3, color),
                  Bit.Var(c[2], 0, color),
                  Bit.Var(c[3], 0, color),
-                 Bit.Var(c[2], 1, color),
-                 Bit.Var(c[3], 1, color),
                  Bit.Var(c[2], 2, color),
                  Bit.Var(c[3], 2, color),
+                 Bit.Var(c[2], 1, color),
+                 Bit.Var(c[3], 1, color),
                  Bit.Var(c[2], 3, color),
                  Bit.Var(c[3], 3, color)])
 
-  l0 = MakeWord("abcd", color="31m")
-  l1 = MakeWord("efgh", color="32m")
-  l2 = MakeWord("ijkl", color="33m")
-  l3 = MakeWord("mnop", color="34m")
+  A = MakeArray(MakeWord)
+  N = len(A)
+  printall("Data:", *A)
 
-  printall("Data:", l0, l1, l2, l3)
+  B = FreshArray(N, 16)
+  Blit(lambda a, b: ((a >> 8) & m0) | (b & ~m0),
+       N / 4, 2, Channel(A, 2, 2), Channel(A, 0, 2), Channel(B, 0, 2))
+  Blit(lambda a, b: ((a << 8) & ~m0) | (b & m0),
+       N / 4, 2, Channel(A, 0, 2), Channel(A, 2, 2), Channel(B, 2, 2))
+  printall("Swap 8x4:", *B)
 
-  a0 = (l0 & m1) | (l2.lsr(8) & m0)
-  a1 = (l1 & m1) | (l3.lsr(8) & m0)
-  a2 = (l2 & m0) | (l0.lsl(8) & m1)
-  a3 = (l3 & m0) | (l1.lsl(8) & m1)
+  C = FreshArray(N, 16)
+  Blit(lambda a, b: ((a >> 4) & m1) | (b & ~m1),
+       N / 2, 1, Channel(B, 1, 1), Channel(B, 0, 1), Channel(C, 0, 1))
+  Blit(lambda a, b: ((a << 4) & ~m1) | (b & m1),
+       N / 2, 1, Channel(B, 0, 1), Channel(B, 1, 1), Channel(C, 1, 1))
+  printall("Swap 4x2:", *C)
 
-  printall("Swap 8x4:", a0, a1, a2, a3)
-
-  b0 = (a0 & m3) | (a1.lsr(4) & m2)
-  b1 = (a1 & m2) | (a0.lsl(4) & m3)
-  b2 = (a2 & m3) | (a3.lsr(4) & m2)
-  b3 = (a3 & m2) | (a2.lsl(4) & m3)
-
-  printall("Swap 4x2:", b0, b1, b2, b3)
-
-  c0 = (b0 & m5) | (b2.lsr(2) & m4)
-  c1 = (b2 & m4) | (b0.lsl(2) & m5)
-  c2 = (b1 & m5) | (b3.lsr(2) & m4)
-  c3 = (b3 & m4) | (b1.lsl(2) & m5)
-
-  printall("Swap 2x2:", c0, c1, c2, c3)
+  if bitplane_output:
+    D = FreshBitplanes(4, N / 4, 16)
+    Blit(lambda a, b: ((a >> 2) & m2) | (b & ~m2),
+         N / 4, 1, Channel(C, 2, 3), Channel(C, 0, 3), Channel(D[0], 0, 0))
+    Blit(lambda a, b: ((a >> 2) & m2) | (b & ~m2),
+         N / 4, 1, Channel(C, 3, 3), Channel(C, 1, 3), Channel(D[1], 0, 0))
+    Blit(lambda a, b: ((a << 2) & ~m2) | (b & m2),
+         N / 4, 1, Channel(C, 0, 3), Channel(C, 2, 3), Channel(D[2], 0, 0))
+    Blit(lambda a, b: ((a << 2) & ~m2) | (b & m2),
+         N / 4, 1, Channel(C, 1, 3), Channel(C, 3, 3), Channel(D[3], 0, 0))
+    print("Bitplanes:")
+    printall("[0]:", *D[0])
+    printall("[1]:", *D[1])
+    printall("[2]:", *D[2])
+    printall("[3]:", *D[3])
+  else:
+    D = FreshArray(N, 16)
+    Blit(lambda a, b: ((a >> 2) & m2) | (b & ~m2),
+         N / 4, 2, Channel(C, 2, 2), Channel(C, 0, 2), Channel(D, 0, 2))
+    Blit(lambda a, b: ((a << 2) & ~m2) | (b & m2),
+         N / 4, 2, Channel(C, 0, 2), Channel(C, 2, 2), Channel(D, 2, 2))
+    printall("Swap 2x2:", *D)
 
 
 def c2p_2_1_4_mangled():
@@ -363,15 +431,15 @@ def c2p_2_1_4_mangled():
 
   printall("Data:", l0, l1, l2, l3)
 
-  a = ((l0 & m1) | (l1.lsr(4) & m0)) + ((l2 & m1) | (l3.lsr(4) & m0))
-  b = ((l0.lsl(4) & m1) | (l1 & m0)) + ((l2.lsl(4) & m1) | (l3 & m0))
+  a = ((l0 & m1) | ((l1 >> 4) & m0)) + ((l2 & m1) | ((l3 >> 4) & m0))
+  b = (((l0 << 4) & m1) | (l1 & m0)) + (((l2 << 4) & m1) | (l3 & m0))
 
   printall("Swap 4x2:", a, b)
 
-  x = (a & m2) | (a.lsr(1) & m3)
-  y = (a & m3) | (a.lsl(1) & m2)
-  z = (b & m2) | (b.lsr(1) & m3)
-  w = (b & m3) | (b.lsl(1) & m2)
+  x = (a & m2) | ((a >> 1) & m3)
+  y = (a & m3) | ((a >> 1) & m2)
+  z = (b & m2) | ((b >> 1) & m3)
+  w = (b & m3) | ((b >> 1) & m2)
 
   printall("Bitplanes:", x, y, z, w)
 
@@ -441,7 +509,7 @@ def c2p_2_1_4_stingray():
   printall("Bitplanes:", b0, b1, b2, b3, b4, b5, b6, b7)
 
 
-c2p_1_1_4_accurate()
-#c2p_1_1_4_mangled()
+c2p_1_1_4_blitter()
+#c2p_1_1_4_blitter_mangled()
 #c2p_2_1_4_mangled()
 #c2p_2_1_4_stingray()
