8a6207c578efdbebbcc1c1036b5e6f7073c27008
Krystian Bacławski <krystian.baclawski@gmail.com>
2015-03-14 13:01:59 +0100

    Edge drawing routine using CPU.

--- a500/sys/line.c
+++ a500/sys/line.c
@@ -10,9 +10,9 @@ __regargs void CpuLineSetup(BitmapT *bitmap, UWORD plane) {
   line.stride = bitmap->bytesPerRow;
 }
 
-__regargs void CpuLine(WORD xs, WORD ys, WORD xe, WORD ye) {
-  UBYTE *pixels = line.pixels;
-  LONG stride = line.stride;
+void CpuLine(WORD xs asm("d0"), WORD ys asm("d1"), WORD xe asm("d2"), WORD ye asm("d3")) {
+  register UBYTE *pixels asm("a0") = line.pixels;
+  register LONG stride asm("a1") = line.stride;
   UBYTE color;
   WORD dx, dy;
 
@@ -106,6 +106,90 @@ __regargs void CpuLine(WORD xs, WORD ys, WORD xe, WORD ye) {
           dg += dg2;
         }
       } while (--dx != -1);
+    }
+  }
+}
+
+void CpuEdge(WORD x1 asm("d0"), WORD y1 asm("d1"), WORD x2 asm("d2"), WORD y2 asm("d3")) {
+  register UBYTE *pixels asm("a0") = line.pixels;
+  register LONG stride asm("a1") = line.stride;
+
+  if (y1 == y2)
+    return;
+
+  {
+    WORD dy = y2 - y1;
+    WORD dx = x2 - x1;
+    UBYTE color = 0x80 >> (x1 & 7);
+
+    pixels += (LONG)((WORD)y1 * (WORD)stride);
+    pixels += x1 >> 3;
+
+    if (dx < 0)
+      dx = -dx;
+
+    if (dx <= dy) {
+      WORD err = dy / 2;
+      WORD n = dy - 1;
+
+      if (x1 <= x2) {
+        do {
+          *pixels ^= color;
+          pixels += stride;
+          err += dx;
+          if (err >= dy) {
+            asm("rorb  #1,%0\n"
+                "bccs  . +4\n"
+                "addql #1,%1\n"
+                : "+d" (color), "+a" (pixels));
+            err -= dy;
+          }
+        } while (--n != -1);
+      } else {
+        do {
+          *pixels ^= color;
+          pixels += stride;
+          err += dx;
+          if (err >= dy) {
+            asm("rolb  #1,%0\n"
+                "bccs  . +4\n"
+                "subql #1,%1\n"
+                : "+d" (color), "+a" (pixels));
+            err -= dy;
+          }
+        } while (--n != -1);
+      } 
+    } else {
+      WORD err = dx / 2; /* Makes line symmetric on both ends... I guess */
+      WORD n = dx - 1;
+
+      if (x1 <= x2) {
+        do {
+          err += dy;
+          if (err >= dx) {
+            *pixels ^= color;
+            pixels += stride;
+            err -= dx;
+          }
+          asm("rorb  #1,%0\n"
+              "bccs  . +4\n"
+              "addql #1,%1\n"
+              : "+d" (color), "+a" (pixels));
+        } while (--n != -1);
+      } else {
+        do {
+          err += dy;
+          if (err >= dx) {
+            *pixels ^= color;
+            pixels += stride;
+            err -= dx;
+          }
+          asm("rolb  #1,%0\n"
+              "bccs  . +4\n"
+              "subql #1,%1\n"
+              : "+d" (color), "+a" (pixels));
+        } while (--n != -1);
+      }
     }
   }
 }
--- a500/sys/line.h
+++ a500/sys/line.h
@@ -4,7 +4,7 @@
 #include "gfx.h"
 
 __regargs void CpuLineSetup(BitmapT *bitmap, UWORD plane);
-__regargs void CpuLine(WORD x0, WORD y0, WORD x1, WORD y1);
-__regargs void CpuLineEdge(WORD x1, WORD y1, WORD x2, WORD y2);
+void CpuLine(WORD x1 asm("d0"), WORD y1 asm("d1"), WORD x2 asm("d2"), WORD y2 asm("d3"));
+void CpuEdge(WORD x1 asm("d0"), WORD y1 asm("d1"), WORD x2 asm("d2"), WORD y2 asm("d3"));
 
 #endif
