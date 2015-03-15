ddaae4bfdc64e5cc508b6d9f442af3e500bd165b
Krystian Bacławski <krystian.baclawski@gmail.com>
2015-03-15 20:33:28 +0100

    Fix identity matrix(!). Rework matrix composition.

--- a500/sys/3d-transform.c
+++ a500/sys/3d-transform.c
@@ -12,10 +12,10 @@ __regargs void LoadIdentity3D(Matrix3D *M) {
   M->m12 = 0;
   M->y = 0;
 
-  M->m10 = 0;
-  M->m11 = 0;
-  M->m12 = fx12f(1.0);
-  M->y = 0;
+  M->m20 = 0;
+  M->m21 = 0;
+  M->m22 = fx12f(1.0);
+  M->z = 0;
 }
 
 __regargs void Translate3D(Matrix3D *M, WORD x, WORD y, WORD z) {
@@ -36,39 +36,6 @@ __regargs void Scale3D(Matrix3D *M, WORD sx, WORD sy, WORD sz) {
   M->m20 = normfx(M->m20 * sx);
   M->m21 = normfx(M->m21 * sy);
   M->m22 = normfx(M->m22 * sz);
-}
-
-#define MULROW() {            \
-  WORD *c = a;                \
-  LONG t0 = (*c++) * (*b++);  \
-  LONG t1 = (*c++) * (*b++);  \
-  LONG t2 = (*c++) * (*b++);  \
-  *m++ = normfx(t0 + t1 + t2); \
-}
-
-/* Assumes "b" is a transposed 3x3 matrix! */
-static __regargs void Compose3D(Matrix3D *d, WORD a[9], WORD b[9]) {
-  WORD *m = &d->m00;
-
-  pushl(b);
-  MULROW();
-  MULROW();
-  MULROW();
-  popl(b);
-  a += 3;
-  m++;
-
-  pushl(b);
-  MULROW();
-  MULROW();
-  MULROW();
-  popl(b);
-  a += 3;
-  m++;
-
-  MULROW();
-  MULROW();
-  MULROW();
 }
 
 __regargs void LoadRotate3D(Matrix3D *M, WORD ax, WORD ay, WORD az) {
@@ -98,42 +65,61 @@ __regargs void LoadRotate3D(Matrix3D *M, WORD ax, WORD ay, WORD az) {
   M->z = 0;
 }
 
-__regargs void Rotate3D(Matrix3D *M, WORD ax, WORD ay, WORD az) {
-  WORD ma[9], mb[9];
+#define MULROW() {             \
+  LONG t0 = (*a++) * (*b0++);  \
+  LONG t1 = (*a++) * (*b1++);  \
+  LONG t2 = (*a++) * (*b2++);  \
+  *d++ = normfx(t0 + t1 + t2); \
+}
 
-  ma[0] = M->m00;
-  ma[1] = M->m01;
-  ma[2] = M->m02;
-  ma[3] = M->m10;
-  ma[4] = M->m11;
-  ma[5] = M->m12;
-  ma[6] = M->m20;
-  ma[7] = M->m21;
-  ma[8] = M->m22;
+__regargs void Compose3D(Matrix3D *md, Matrix3D *ma, Matrix3D *mb) {
+  WORD *a = &ma->m00;
+  WORD *d = &md->m00;
 
   {
-    WORD sinX = SIN(ax);
-    WORD cosX = COS(ax);
-    WORD sinY = SIN(ay);
-    WORD cosY = COS(ay);
-    WORD sinZ = SIN(az);
-    WORD cosZ = COS(az);
+    WORD *b0 = &mb->m00;
+    WORD *b1 = &mb->m10;
+    WORD *b2 = &mb->m20;
 
-    WORD tmp0 = normfx(sinX * sinY);
-    WORD tmp1 = normfx(cosX * sinY);
+    MULROW(); a -= 3;
+    MULROW(); a -= 3;
+    MULROW();
 
-    mb[0] = normfx(cosY * cosZ);
-    mb[3] = normfx(cosY * sinZ);
-    mb[6] = -sinY;
-    mb[1] = normfx(tmp0 * cosZ - cosX * sinZ);
-    mb[4] = normfx(tmp0 * sinZ + cosX * cosZ);
-    mb[7] = normfx(sinX * cosY);
-    mb[2] = normfx(tmp1 * cosZ + sinX * sinZ);
-    mb[5] = normfx(tmp1 * sinZ - sinX * cosZ);
-    mb[8] = normfx(cosX * cosY);
+    *d++ = (*a++) + (*b0);
   }
 
-  Compose3D(M, ma, mb);
+  {
+    WORD *b0 = &mb->m00;
+    WORD *b1 = &mb->m10;
+    WORD *b2 = &mb->m20;
+
+    MULROW(); a -= 3;
+    MULROW(); a -= 3;
+    MULROW();
+
+    *d++ = (*a++) + (*b1);
+  }
+
+  {
+    WORD *b0 = &mb->m00;
+    WORD *b1 = &mb->m10;
+    WORD *b2 = &mb->m20;
+
+    MULROW(); a -= 3;
+    MULROW(); a -= 3;
+    MULROW();
+
+    *d++ = (*a++) + (*b2);
+  }
+}
+
+__regargs void Rotate3D(Matrix3D *M, WORD ax, WORD ay, WORD az) {
+  Matrix3D tmp, rot;
+
+  LoadRotate3D(&rot, ax, ay, az);
+  memcpy(&tmp, M, sizeof(Matrix3D));
+
+  Compose3D(M, &tmp, &rot);
 }
 
 #define MULVERTEX() {                 \
