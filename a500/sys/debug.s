6d7e0b73b46abdc80f9e9ac02646ac56e0458680
Krystian Bacławski <krystian.baclawski@gmail.com>
2016-01-25 01:27:07 +0100

    Move serial print routines to separate file.

---
+++ a500/sys/debug.s
@@ -0,0 +1,64 @@
+        xdef    _KPutChar
+        xdef    _KPutByte
+        xdef    _KPutWord
+        xdef    _KPutLong
+        xdef    _KPutStr
+
+        include 'hardware/custom.i'
+
+        section "code",code
+
+_KPutChar:
+        and.w   #$ff,d0
+        or.w    #$100,d0
+
+.loop   btst    #5,serdatr+$dff000
+        beq     .loop
+
+        move.w  d0,serdat+$dff000
+
+        cmp.b   #10,d0  ; '\n'
+        bne     .quit
+
+        addq.w  #3,d0   ; '\r'
+        bra     .loop
+
+.quit:  rts
+
+_KPutByte:
+        ror.l   #8,d0
+        moveq   #1,d1
+        bra     KPutInt
+
+_KPutWord:
+        swap    d0
+        moveq   #3,d1
+        bra     KPutInt
+
+_KPutLong:
+        moveq   #7,d1
+
+KPutInt:
+        lea     hex(pc),a0
+
+.loop   rol.l   #4,d0
+        move.l  d0,-(sp)
+        and.w   #$000f,d0
+        move.b  (a0,d0.w),d0
+        bsr     _KPutChar
+        move.l  (sp)+,d0
+        dbf     d1,.loop
+
+        rts
+
+hex:    dc.b    '0123456789ABCDEF'
+
+_KPutStr:
+.loop   move.b  (a0)+,d0
+        bne     .putc
+        rts
+
+.putc   bsr     _KPutChar
+        bra     .loop
+
+; vim: ft=asm68k:ts=8:sw=8
