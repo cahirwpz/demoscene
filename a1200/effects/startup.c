2f398ce631d6422d7eb91da40d5a8255b2d5ae5f
Krystian Bacławski <krystian.baclawski@gmail.com>
2016-05-05 10:27:14 +0200

    Check procedure is part of startup... and nothing else.

--- a1200/effects/startup.c
+++ a1200/effects/startup.c
@@ -1,5 +1,10 @@
+#include <proto/dos.h>
+#include <proto/exec.h>
+#include <proto/graphics.h>
+#include <exec/execbase.h>
+#include <graphics/gfxbase.h>
+
 #include "std/exception.h"
-#include "system/check.h"
 #include "system/display.h"
 #include "system/input.h"
 
@@ -8,6 +13,21 @@
 int __nocommandline = 1;
 
 extern EffectT Effect;
+
+static bool SystemCheck() {
+  bool kickv40 = SysBase->LibNode.lib_Version >= 40;
+  bool chipaga = GfxBase->ChipRevBits0 & (GFXF_AA_ALICE|GFXF_AA_LISA);
+  bool cpu68060 = SysBase->AttnFlags & AFF_68060;
+  bool fpu68882 = SysBase->AttnFlags & AFF_68882;
+
+  Printf("System check:\r\n");
+  Printf(" - Kickstart v40 : %s\r\n", (ULONG)(kickv40 ? "yes" : "no"));
+  Printf(" - ChipSet AGA : %s\r\n", (ULONG)(chipaga ? "yes" : "no"));
+  Printf(" - CPU 68060 : %s\r\n", (ULONG)(cpu68060 ? "yes" : "no"));
+  Printf(" - FPU 68882 : %s\r\n", (ULONG)(fpu68882 ? "yes" : "no"));
+
+  return (kickv40 && cpu68060 && fpu68882 && chipaga);
+}
 
 int main() {
   if (SystemCheck()) {
--- a1200/system/check.h
+++
@@ -1,8 +0,0 @@
-#ifndef __SYSTEM_CHECK_H__
-#define __SYSTEM_CHECK_H__
-
-#include "std/types.h"
-
-bool SystemCheck();
-
-#endif
