4a9e1abe0a12ea7864e65fcd3e1e5c128b6cbf05
Krystian Bacławski <krystian.baclawski@gmail.com>
2017-10-19 09:11:07 +0200

    Apply delta encoding to sintab to make it packable.

--- a500/base/libsys/sintab.py
+++ a500/base/libsys/sintab.py
@@ -3,14 +3,27 @@
 from math import sin, pi
 
 if __name__ == '__main__':
-  with open('sintab.s', 'w') as fh:
-    print >>fh, '\tXDEF\t_sintab\n'
-    print >>fh, '\tSECTION\tsintab,DATA\n'
-    print >>fh, '_sintab:'
-
+  with open('sintab.c', 'w') as fh:
     prec = 4096
 
+    print >>fh, '#include "common.h"\n'
+    print >>fh, 'WORD sintab[%d] = {' % prec
+
     sintab = [int(sin(i * 2 * pi / prec) * prec) for i in range(prec)]
+    prev = 0
 
     for a in sintab:
-      print >>fh, '\tdc.w\t%d' % a
+      print >>fh, '\t%d,' % (a - prev)
+      prev = a
+
+    print >>fh, '};'
+    print >>fh, '\nvoid InitSinTab() {'
+    print >>fh, '  WORD sum = 0, n = %d;' % prec
+    print >>fh, '  WORD *tab = sintab;'
+    print >>fh, '  Log("[Init] Preparing sinus table\n");'
+    print >>fh, '  while (--n >= 0) {'
+    print >>fh, '    sum += *tab;'
+    print >>fh, '    *tab++ = sum;'
+    print >>fh, '  }'
+    print >>fh, '}'
+    print >>fh, '\nADD2INIT(InitSinTab, 0);'
