cb74aaf4871d8e78f8488f64296fa25bfa76e046
Krystian Bacławski <krystian.baclawski@gmail.com>
2017-12-05 22:29:22 +0100

    Split out audio library and compile some tests.

--- a500/base/libaudio/audio.c
+++ a500/base/libaudio/audio.c
@@ -1,25 +1,5 @@
 #include "hardware.h"
 #include "sound.h"
-#include "memory.h"
-
-__regargs SoundT *NewSound(ULONG length, UWORD rate) {
-  SoundT *sound = MemAlloc(sizeof(SoundT), MEMF_PUBLIC|MEMF_CLEAR);
-
-  sound->length = length;
-  sound->rate = rate;
-
-  if (length)
-    sound->sample = MemAlloc(sizeof(BYTE) * length, MEMF_CHIP);
-
-  return sound;
-}
-
-__regargs void DeleteSound(SoundT *sound) {
-  if (sound) {
-    MemFree(sound->sample);
-    MemFree(sound);
-  }
-}
 
 void AudioSetVolume(ChanT num, UBYTE level) {
   custom->aud[num].ac_vol = (UWORD)level;
