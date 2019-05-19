04f00a3ba183bde6008955ac1d9b6d9fb89fcb4c
Krzysztof Jaroma <krzysztof.jaroma@gmail.com>
2019-05-19 23:49:01 +0200

    Convert colortab to Python3

--- a500/base/libgfx/colortab.py
+++ a500/base/libgfx/colortab.py
@@ -1,13 +1,14 @@
-#!/usr/bin/env python -B
+#!/usr/bin/env python3
+from contextlib import redirect_stdout
 
 if __name__ == '__main__':
   with open('colortab.s', 'w') as fh:
-    print >>fh, '\tXDEF\t_colortab\n'
-    print >>fh, '\tSECTION\tcolortab,DATA\n'
-    print >>fh, '_colortab:'
-
-    for i in range(16):
-      for j in range(16):
-        row = ', '.join(["$%.2x" % ((i + (j - i) * k / 15) * 16)
-                         for k in range(16)])
-        print >>fh, '\tdc.b\t' + row
+    with redirect_stdout(fh):
+        print('\tXDEF\t_colortab\n')
+        print('\tSECTION\tcolortab,DATA\n')
+        print('_colortab:')
+        
+        for i in range(16):
+          for j in range(16):
+            row = ', '.join(["${:02x}".format((i + (j - i) * k // 15) * 16) for k in range(16)])
+            print('\tdc.b\t' + row)
