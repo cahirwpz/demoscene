0583a4d81a35933cd92f07e1ce4f003d8d0bf459
Krzysztof Jaroma <krzysztof.jaroma@gmail.com>
2019-05-19 14:50:33 +0200

    Use redirect_stdout in sintab for better code readability

--- a500/base/libsys/sintab.py
+++ a500/base/libsys/sintab.py
@@ -1,29 +1,31 @@
 #!/usr/bin/env python3
 
 from math import sin, pi
+from contextlib import redirect_stdout
 
 if __name__ == '__main__':
   with open('sintab.c', 'w') as fh:
-    prec = 4096
+    with redirect_stdout(fh):
+      prec = 4096
 
-    print('#include "common.h"\n', file=fh)
-    print('WORD sintab[%d] = {' % prec, file=fh)
+      print('#include "common.h"\n')
+      print('WORD sintab[%d] = {' % prec)
 
-    sintab = [int(sin(i * 2 * pi / prec) * prec) for i in range(prec)]
-    prev = 0
+      sintab = [int(sin(i * 2 * pi / prec) * prec) for i in range(prec)]
+      prev = 0
 
-    for a in sintab:
-      print('\t%d,' % (a - prev), file=fh)
-      prev = a
+      for a in sintab:
+        print('\t%d,' % (a - prev))
+        prev = a
 
-    print('};', file=fh)
-    print('\nvoid InitSinTab() {', file=fh)
-    print('  WORD sum = 0, n = %d;' % prec, file=fh)
-    print('  WORD *tab = sintab;', file=fh)
-    print('  Log("[Init] Preparing sinus table\n");', file=fh)
-    print('  while (--n >= 0) {', file=fh)
-    print('    sum += *tab;', file=fh)
-    print('    *tab++ = sum;', file=fh)
-    print('  }', file=fh)
-    print('}', file=fh)
-    print('\nADD2INIT(InitSinTab, 0);', file=fh)
+      print('};')
+      print('\nvoid InitSinTab() {')
+      print('  WORD sum = 0, n = %d;' % prec)
+      print('  WORD *tab = sintab;')
+      print('  Log("[Init] Preparing sinus table\n");')
+      print('  while (--n >= 0) {')
+      print('    sum += *tab;')
+      print('    *tab++ = sum;')
+      print('  }')
+      print('}')
+      print('\nADD2INIT(InitSinTab, 0);')
