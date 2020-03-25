328a25b98586aa9d9fee55ad655dbc8e95f02913
Krystian Bacławski <krystian.baclawski@gmail.com>
2020-03-25 22:37:12 +0100

    Python files should generate C source files.

--- effects/bumpmap-rgb/data/bumpmap.py
+++ effects/bumpmap-rgb/data/bumpmap.py
@@ -1,39 +1,35 @@
 #!/usr/bin/env python3
 
-import sys
-import os
 from PIL import Image
-from array import array
 from utils import constrain
+import sys
 
 
-if __name__ == "__main__":
-  image = Image.open(sys.argv[1])
-  image = image.convert("L")
+if __name__ == '__main__':
+    image = Image.open(sys.argv[1])
+    image = image.convert('L')
 
-  bumpmap = array("H")
-  name = os.path.splitext(sys.argv[1])[0] + ".bin"
+    print('u_short %s[] = {' % sys.argv[2])
 
-  for y in range(image.size[1]):
-    for x in range(image.size[0]):
-      this = image.getpixel((x, y))
-      if y < image.size[1] - 1:
-        down = image.getpixel((x, y + 1))
-      else:
-        down = this
-      if x < image.size[0] - 1:
-        right = image.getpixel((x + 1, y))
-      else:
-        right = this
-      # scale down the difference between pixels
-      du = (this - down) * 0.25
-      dv = (this - right) * 0.25
-      # light texture size is 128 * 128
-      u = (constrain(int(du), -64, 63) + y) & 127
-      v = (constrain(int(dv), -64, 63) + x) & 127
-      bumpmap.append(u * 128 + v)
+    for y in range(image.size[1]):
+        sys.stdout.write(' ')
+        for x in range(image.size[0]):
+            this = image.getpixel((x, y))
+            if y < image.size[1] - 1:
+                down = image.getpixel((x, y + 1))
+            else:
+                down = this
+            if x < image.size[0] - 1:
+                right = image.getpixel((x + 1, y))
+            else:
+                right = this
+            # scale down the difference between pixels
+            du = (this - down) * 0.25
+            dv = (this - right) * 0.25
+            # light texture size is 128 * 128
+            u = (constrain(int(du), -64, 63) + y) & 127
+            v = (constrain(int(dv), -64, 63) + x) & 127
+            sys.stdout.write(' 0x%04x,' % (u * 128 + v))
+        sys.stdout.write('\n')
 
-  bumpmap.byteswap()
-
-  with open(name, "wb") as f:
-    bumpmap.tofile(f)
+    print('};')
