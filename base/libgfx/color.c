a12ba7decfe6b00b70dc01add4ef9cd9fa49cf4a
Krystian Bacławski <krystian.baclawski@gmail.com>
2020-04-04 21:44:26 +0200

    Remove contrast functions.

--- base/libgfx/color.c
+++ base/libgfx/color.c
@@ -28,36 +28,4 @@ __regargs u_short ColorTransition(u_short from, u_short to, u_short step) {
   return (colortab[r] << 4) | colortab[g] | (colortab[b] >> 4);
 }
 
-__regargs u_short ColorIncreaseContrastRGB(short r, short g, short b, u_short step) {
-  r &= 0xf0;
-  r += colortab[(short)(r | step)];
-  if (r > 0xf0) r = 0xf0;
-
-  g &= 0xf0;
-  g += colortab[(short)(g | step)];
-  if (g > 0xf0) g = 0xf0;
-
-  b &= 0xf0;
-  b += colortab[(short)(b | step)];
-  if (b > 0xf0) b = 0xf0;
-
-  return (r << 4) | g | (b >> 4);
-}
-
-__regargs u_short ColorDecreaseContrastRGB(short r, short g, short b, u_short step) {
-  r &= 0xf0;
-  r -= colortab[(short)(r | step)];
-  if (r < 0) r = 0;
-
-  g &= 0xf0;
-  g -= colortab[(short)(g | step)];
-  if (g < 0) g = 0;
-
-  b &= 0xf0;
-  b -= colortab[(short)(b | step)];
-  if (b < 0) b = 0;
-
-  return (r << 4) | g | (b >> 4);
-}
-
 ADD2INIT(InitColorTab, 0);
