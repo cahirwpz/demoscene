3207f07429659bc9b1824dfb45a937012970bc8d
Krystian Bacławski <krystian.baclawski@gmail.com>
2020-04-04 23:41:38 +0200

    Split out bitmap.h and palette.h from gfx.h

--- base/include/gfx.h
+++ base/include/gfx.h
@@ -1,12 +1,7 @@
-#ifndef __BITMAP_H__
-#define __BITMAP_H__
+#ifndef __GFX_H__
+#define __GFX_H__
 
 #include "common.h"
-
-typedef struct Palette {
-  u_short count;
-  u_short colors[0];
-} PaletteT;
 
 typedef struct {
   short x, y;
@@ -31,44 +26,7 @@ typedef struct {
   short w, h;
 } Area2D;
 
-/* Size of extra buffer after last word of bitplanes. */
-#define BM_EXTRA sizeof(u_short)
-
-/* Flags stored in Bitmap structure. */
-#define BM_CLEAR        0x01
-#define BM_DISPLAYABLE  0x02
-#define BM_INTERLEAVED  0x04
-#define BM_MINIMAL      0x08
-#define BM_HAM          0x10
-#define BM_EHB          0x20
-#define BM_STATIC       0x40 /* bitplanes were allocated statically */
-#define BM_FLAGMASK     0x7F
-
-/* Maximum number of bitplanes kept in bitmap structure. */
-#define BM_NPLANES 7
-
-typedef struct Bitmap {
-  u_short width;
-  u_short height;
-  u_short depth;
-  u_short bytesPerRow;
-  u_short bplSize;
-  u_char flags;
-  void *planes[BM_NPLANES];
-} BitmapT;
-
-__regargs void InitSharedBitmap(BitmapT *bitmap, u_short width, u_short height,
-                                u_short depth, BitmapT *donor);
-
-__regargs BitmapT *NewBitmapCustom(u_short width, u_short height, u_short depth,
-                                   u_char flags);
-__regargs void DeleteBitmap(BitmapT *bitmap);
-__regargs void BitmapMakeDisplayable(BitmapT *bitmap);
-__regargs bool ClipArea(const Box2D *space, Point2D *pos, Area2D *area);
-__regargs bool InsideArea(short x, short y, Area2D *area);
-
-static inline BitmapT *NewBitmap(u_short width, u_short height, u_short depth) {
-  return NewBitmapCustom(width, height, depth, BM_CLEAR|BM_DISPLAYABLE);
-}
+#include "bitmap.h"
+#include "palette.h"
 
 #endif
