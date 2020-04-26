95caa01b24e065c8e2072754b662cdc2f4e62e72
Krzysztof Jaroma <krzysztof.jaroma@gmail.com>
2020-04-26 22:05:08 +0200

    Add custom color background as a cli flag

--- tools/makepchg/main.go
+++ tools/makepchg/main.go
@@ -3,11 +3,26 @@ package main
 import (
 	"../misc"
 	"flag"
+	"fmt"
 	"image"
+	"image/color"
 	"image/draw"
 	"log"
 	"sort"
 )
+
+func parseHexColor(s string) (c color.RGBA, err error) {
+	c.A = 0xff
+	if len(s) == 3 {
+		_, err = fmt.Sscanf(s, "%1x%1x%1x", &c.R, &c.G, &c.B)
+		c.R *= 17
+		c.G *= 17
+		c.B *= 17
+	} else {
+		err = fmt.Errorf("invalid color length, must be 3")
+	}
+	return
+}
 
 func exportImage(baseName string, img *image.Paletted,
 	pxMap map[int][]uint8) (err error) {
@@ -19,27 +34,42 @@ func exportImage(baseName string, img *image.Paletted,
 	}
 
 	dim := image.Rect(0, 0, rowMax, img.Bounds().Dy())
-	palImg := image.NewPaletted(dim, img.Palette)
+	palImg := image.NewRGBA(dim)
+	fill := image.NewRGBA(image.Rect(0, 0, palImg.Bounds().Dx()+1,
+		img.Bounds().Dy()))
+	fillCol := color.RGBA{0, 255, 0, 255}
+	if len(col) != 0 {
+		fillCol, err = parseHexColor(col)
+		if err != nil {
+			return err
+		}
+	}
+	draw.Draw(fill, image.Rect(0, 0, fill.Bounds().Dx(),
+		fill.Bounds().Dy()),
+		&image.Uniform{C: fillCol},
+		image.Point{},
+		draw.Over)
+
 	for y := 0; y < palImg.Bounds().Dy(); y++ {
+		rowLen := len(pxMap[y])
 		for x := 0; x < palImg.Bounds().Dx(); x++ {
-			rowLen := len(pxMap[y])
-			var px uint8
-			if x > rowLen-1 {
-				px = 0
+			var px color.Color
+			if x < rowLen {
+				px = img.Palette[pxMap[y][x]]
 			} else {
-				px = pxMap[y][x]
+				px = fillCol
 			}
-			palImg.SetColorIndex(x, y, px)
+			fill.Set(x+1, y, px)
 		}
 	}
 
 	outDim := image.Rect(0, 0,
-		img.Bounds().Dx()+rowMax, img.Bounds().Dy())
-	out := image.NewPaletted(outDim, img.Palette)
+		img.Bounds().Dx()+fill.Bounds().Dx(), img.Bounds().Dy())
+	out := image.NewRGBA(outDim)
 
 	draw.Draw(out,
-		image.Rect(img.Bounds().Dx(), 0, img.Bounds().Dx()+palImg.Bounds().Dx(),
-			img.Bounds().Dy()), palImg, image.Point{}, draw.Over)
+		image.Rect(img.Bounds().Dx(), 0, img.Bounds().Dx()+fill.Bounds().
+			Dx(), img.Bounds().Dy()), fill, image.Point{}, draw.Over)
 	draw.Draw(out, image.Rect(0, 0,
 		img.Bounds().Dx(), img.Bounds().Dy()),
 		img, image.Point{}, draw.Over)
@@ -47,6 +77,13 @@ func exportImage(baseName string, img *image.Paletted,
 	misc.SavePNG(baseName+"_pal.png", out)
 
 	return
+}
+
+var col string
+
+func init() {
+	flag.StringVar(&col, "color", "",
+		"Sets palette background, color format 'fad'")
 }
 
 func main() {
