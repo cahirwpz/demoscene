c8806d803044887f81274a770e984bd3e6d17059
losso <losso@heckmeck.de>
2020-05-19 21:54:24 +0200

    Fix Python code style

--- effects/butterfly-gears/data/gen-unrolled-loop.py
+++ effects/butterfly-gears/data/gen-unrolled-loop.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
         raise SystemExit(f'Usage: {sys.argv[0]} source.txt')
     txtFile = sys.argv[1]
     chunks = {}
-    chunk  = None
+    chunk = None
 
     with open(txtFile) as f:
         for line in f:
@@ -19,7 +19,8 @@ if __name__ == '__main__':
                     chunks[chunk] += line
                 else:
                     chunks[chunk] = line
-    for key in ['startrow','endrow','plot-even','plot-odd','add-dst','addq-dst','map']:
+    for key in ['startrow', 'endrow', 'plot-even', 'plot-odd', 'add-dst',
+                'addq-dst', 'map']:
         if key not in chunks:
             raise SystemExit(f'Chunk "{key}" not found in {txtFile}')
 
@@ -32,8 +33,10 @@ if __name__ == '__main__':
         for ch in line:
             if ch == '#':
                 if dst_inc > 0:
-                    dst_inc_chunk = 'addq-dst' if dst_inc <= 8 and not crossedrow else 'add-dst'
-                    dst_value     = f'{dst_inc}+SINGLEROW_SIZE' if crossedrow else str(dst_inc)
+                    dst_inc_chunk = 'addq-dst' \
+                        if dst_inc <= 8 and not crossedrow else 'add-dst'
+                    dst_value = f'{dst_inc}+SINGLEROW_SIZE' \
+                        if crossedrow else str(dst_inc)
                     print(chunks[dst_inc_chunk].replace('$BYTES', dst_value))
                     crossedrow = False
                     dst_inc = 0
@@ -44,9 +47,11 @@ if __name__ == '__main__':
                     dst_inc += 4
             elif ch == '.':
                 print(chunks['plot-skip'])
-                if not even: dst_inc += 4
+                if not even:
+                    dst_inc += 4
             elif ch == ':':
-                if not even: dst_inc += 4
+                if not even:
+                    dst_inc += 4
             elif ch == ' ':
                 break
             else:
