cce81184917ad174509afdd448e070f1303f8d33
losso <losso@heckmeck.de>
2020-05-24 12:03:14 +0200

    Y movement; ball structs

--- effects/butterfly-gears/butterfly-gears.c
+++ effects/butterfly-gears/butterfly-gears.c
@@ -15,6 +15,7 @@
 #include "event.h"
 
 #define DEPTH 5
+#define BALLS 3
 #define ROTZOOM_W 24
 #define ROTZOOM_H 24
 #define COPWAIT_X 1
@@ -48,20 +49,20 @@ typedef struct {
   short vDelta;
   short screenX;
   short screenY;
+  short screenLineHeight;
 } BallT;
 
-// TODO arrays
+typedef struct {
+  CopInsT *waitBefore;
+  CopInsT *waitAfter;
+  CopInsT *ballCopper;
+  CopInsT *bplptr[DEPTH];
+  CopInsT *bplcon1ins;
+} BallCopInsertsT;
+
 typedef struct {
   CopListT *cp;
-  CopInsT *upperBallCopper;
-  CopInsT *lowerBallCopper;
-  CopInsT *lowestBallCopper;
-  CopInsT *bplptrUpper[DEPTH];
-  CopInsT *bplptrLower[DEPTH];
-  CopInsT *bplptrLowest[DEPTH];
-  CopInsT *bplcon1Upper;
-  CopInsT *bplcon1Lower;
-  CopInsT *bplcon1Lowest;
+  BallCopInsertsT inserts[BALLS];
 } BallCopListT;
 
 static BallCopListT ballCopList1; // TODO use second copper list and double-buffer
@@ -125,6 +126,8 @@ static void InitCopperListBall(CopListT *cp, int y, int yInc) {
 }
 
 static void MakeBallCopperList(BallCopListT *ballCp) {
+  short i;
+
   CopListT *cp = NewCopList(INSTRUCTIONS_PER_BALL * 3 + 100);
   ballCp->cp = cp;
   CopInit(cp);
@@ -134,60 +137,69 @@ static void MakeBallCopperList(BallCopListT *ballCp) {
   SET_COLOR(1, 0x000);
   CopSetupBitplaneFetch(cp, MODE_LORES, X(0), ball_large.width);
 
-  CopSetupBitplanes(cp, ballCp->bplptrUpper, &ball_large, ball_large.depth);
-  ballCp->bplcon1Upper = CopMove16(cp, bplcon1, 0);
-  CopSetupMode(cp, MODE_LORES, ball_large.depth);
-  ballCp->upperBallCopper = cp->curr;
-  InitCopperListBall(cp, Y0 + 6, 2);
-
-  CopSetupBitplanes(cp, ballCp->bplptrLower, &ball_small, ball_small.depth);
-  ballCp->bplcon1Lower = CopMove16(cp, bplcon1, 0);
-  CopSetupMode(cp, MODE_LORES, ball_small.depth);
-  ballCp->lowerBallCopper = cp->curr;
-  InitCopperListBall(cp, Y0 + 119, 1);
-
-  CopSetupBitplanes(cp, ballCp->bplptrLowest, &ball_small, ball_small.depth);
-  ballCp->bplcon1Lowest = CopMove16(cp, bplcon1, 0);
-  CopSetupMode(cp, MODE_LORES, ball_small.depth);
-  ballCp->lowestBallCopper = cp->curr;
-  InitCopperListBall(cp, Y0 + 182, 1);
+  for (i = 0; i < 1; i++) {
+    ballCp->inserts[i].waitBefore = CopWait(cp, Y0, COPWAIT_X);
+    CopSetupBitplanes(cp, ballCp->inserts[i].bplptr, &ball_large, ball_large.depth);
+    ballCp->inserts[i].bplcon1ins = CopMove16(cp, bplcon1, 0);
+    CopSetupMode(cp, MODE_LORES, ball_large.depth);
+    ballCp->inserts[i].ballCopper = cp->curr;
+    InitCopperListBall(cp, Y0 + 6, 2);
+    ballCp->inserts[i].waitAfter = CopWait(cp, Y0 + 112, COPWAIT_X);
+    CopMove16(cp, bplcon0, BPLCON0_COLOR);
+  }
 
   CopEnd(cp);
 }
 
-static void DrawCopperBall(CopInsT *copper, BallT *ball, CopInsT **bplptr, CopInsT *bplcon1ins) {
-  short sin;
-  short cos;
-  int u;
-  int v;
-  int deltaCol;
-  int deltaRow;
-  int uvPos;
+static void DrawCopperBall(BallT *ball, BallCopInsertsT inserts) {
 
-  short x = 263 - ball->screenX;
-  short bplSkip = (x / 8) & 0xfe;
-  short shift = 15 - (x & 0xf);
-  CopInsSet32(bplptr[0], ball_large.planes[0]+bplSkip);
-  CopInsSet32(bplptr[1], ball_large.planes[1]+bplSkip);
-  CopInsSet32(bplptr[2], ball_large.planes[2]+bplSkip);
-  CopInsSet32(bplptr[3], ball_large.planes[3]+bplSkip);
-  CopInsSet32(bplptr[4], ball_large.planes[4]+bplSkip);
-  CopInsSet16(bplcon1ins, (shift << 4) | shift);
+  {
+    short x = 263 - ball->screenX;
+    short bplSkip = (x / 8) & 0xfe;
+    short shift = 15 - (x & 0xf);
+    CopInsSet32(inserts.bplptr[0], ball_large.planes[0]+bplSkip);
+    CopInsSet32(inserts.bplptr[1], ball_large.planes[1]+bplSkip);
+    CopInsSet32(inserts.bplptr[2], ball_large.planes[2]+bplSkip);
+    CopInsSet32(inserts.bplptr[3], ball_large.planes[3]+bplSkip);
+    CopInsSet32(inserts.bplptr[4], ball_large.planes[4]+bplSkip);
+    CopInsSet16(inserts.bplcon1ins, (shift << 4) | shift);
+  }
 
-  sin = (ball->zoom*SIN(ball->angle)) >> 9;
-  cos = (ball->zoom*COS(ball->angle)) >> 9;
-  deltaCol = uv(sin, cos);
-  deltaRow = uv(cos, -sin);
-  u = ball->u - sin * (ROTZOOM_W / 2) - cos * (ROTZOOM_H / 2);
-  v = ball->v - cos * (ROTZOOM_W / 2) + sin * (ROTZOOM_H / 2);
-  uvPos = uv(u, v);
-  PlotTextureAsm((char*) copper, (char*) ball->texture.pixels, uvPos, deltaCol, deltaRow);
-  ball->angle += ball->angleDelta;
-  ball->u += ball->uDelta;
-  ball->v += ball->vDelta;
-  ball->zoom += ball->zoomDelta;
-  if (ball->zoom < MIN_ZOOM || ball->zoom > MAX_ZOOM) {
-    ball->zoomDelta = -ball->zoomDelta;
+  {
+    short i;
+    short y = ball->screenY;
+    short yInc = ball->screenLineHeight;
+    CopInsT *textureCopper = inserts.ballCopper;
+    inserts.waitBefore->wait.vp = y;
+    y += 6;
+    for (i = 0; i < ROTZOOM_H; i++) {
+      textureCopper->wait.vp = y;
+      textureCopper += ROTZOOM_W / 2 + 2;
+      y += yInc;
+      textureCopper->wait.vp = y;
+      textureCopper += ROTZOOM_W / 2 + 2;
+      y += yInc;
+    }
+  }
+
+  {
+    short sin, cos;
+    int u, v, deltaCol, deltaRow, uvPos;
+    sin = (ball->zoom*SIN(ball->angle)) >> 9;
+    cos = (ball->zoom*COS(ball->angle)) >> 9;
+    deltaCol = uv(sin, cos);
+    deltaRow = uv(cos, -sin);
+    u = ball->u - sin * (ROTZOOM_W / 2) - cos * (ROTZOOM_H / 2);
+    v = ball->v - cos * (ROTZOOM_W / 2) + sin * (ROTZOOM_H / 2);
+    uvPos = uv(u, v);
+    PlotTextureAsm((char *) inserts.ballCopper, (char *) ball->texture.pixels, uvPos, deltaCol, deltaRow);
+    ball->angle += ball->angleDelta;
+    ball->u += ball->uDelta;
+    ball->v += ball->vDelta;
+    ball->zoom += ball->zoomDelta;
+    if (ball->zoom < MIN_ZOOM || ball->zoom > MAX_ZOOM) {
+      ball->zoomDelta = -ball->zoomDelta;
+    }
   }
 }
 
@@ -198,6 +210,7 @@ static void Init(void) {
   CopListActivate(ballCopList1.cp);
 
   ball1.texture = texture_butterfly;
+  ball1.screenLineHeight = 2;
   ball1.angleDelta = 25;
   ball1.u = f2short(64.0f);
   ball1.v = 0;
@@ -206,14 +219,16 @@ static void Init(void) {
   ball1.zoomDelta = 1;
 
   ball2.texture = texture_butterfly;
+  ball1.screenLineHeight = 2;
   ball2.angleDelta = 0;
   ball2.uDelta = f2short(0.5f);
   ball2.vDelta = f2short(-0.3f);
   ball2.zoom = MIN_ZOOM + (MAX_ZOOM - MIN_ZOOM) / 2;
   ball2.zoomDelta = -1;
 
-  ball3.angleDelta = -27;
   ball3.texture = texture_butterfly;
+  ball1.screenLineHeight = 2;
+  ball3.angleDelta = -27;
   ball3.zoom = MIN_ZOOM + (MAX_ZOOM - MIN_ZOOM) * 3 / 2;
   ball3.u = f2short(64.f);
   ball3.vDelta = f2short(-0.2f);
@@ -238,9 +253,11 @@ static void HandleEvent(void) {
 
 static void Render(void) {
   HandleEvent();
-  DrawCopperBall(ballCopList1.upperBallCopper, &ball1, ballCopList1.bplptrUpper, ballCopList1.bplcon1Upper);
-  DrawCopperBall(ballCopList1.lowerBallCopper, &ball2, ballCopList1.bplptrLower, ballCopList1.bplcon1Lower);
-  DrawCopperBall(ballCopList1.lowestBallCopper, &ball3, ballCopList1.bplptrLowest, ballCopList1.bplcon1Lowest);
+  DrawCopperBall(&ball1, ballCopList1.inserts[0]);
+  if (0) {
+    DrawCopperBall(&ball2, ballCopList1.inserts[1]);
+    DrawCopperBall(&ball3, ballCopList1.inserts[2]);
+  }
   TaskWaitVBlank();
 }
 
