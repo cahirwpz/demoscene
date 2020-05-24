6a8bec2ee493d04f3ff49dbcbdb38f0c1feb2b01
Krzysztof Jaroma <krzysztof.jaroma@gmail.com>
2020-05-24 22:20:08 +0200

    Parse mod patterns and export as txt report

--- tools/ptconv/main.go
+++ tools/ptconv/main.go
@@ -5,6 +5,7 @@ import (
 	"bytes"
 	"encoding/binary"
 	"flag"
+	"fmt"
 	"io"
 	"log"
 	"os"
@@ -12,7 +13,87 @@ import (
 )
 
 var moduleReportTemplate = `Module Name: {{.Name}}
+Module Type: {{.Type}}
+Patterns Order:
+{{- range .PatternsOrder}}{{.}}, {{end }}
+Patterns Data: {{range $idx, $pat := .PatternsData}}
+Pattern: {{$idx}}
+{{- range $index, $note := $pat.Notes}}
+{{- if noteNewLine $note}}
+{{noteToString $note}}
+{{- else}}
+{{- noteToString $note}}
+{{- end}}
+{{- end}}
+{{end}}
 `
+
+var PeriodMap = map[uint16]NoteInfo{
+	1712: {C, 0},
+	1616: {CSharp, 0},
+	1525: {D, 0},
+	1440: {DSharp, 0},
+	1357: {E, 0},
+	1281: {F, 0},
+	1209: {FSharp, 0},
+	1141: {G, 0},
+	1077: {GSharp, 0},
+	1017: {A, 0},
+	961:  {ASharp, 0},
+	907:  {B, 0},
+
+	856: {C, 1},
+	808: {CSharp, 1},
+	762: {D, 1},
+	720: {DSharp, 1},
+	678: {E, 1},
+	640: {F, 1},
+	604: {FSharp, 1},
+	570: {G, 1},
+	538: {GSharp, 1},
+	508: {A, 1},
+	480: {ASharp, 1},
+	453: {B, 1},
+
+	428: {C, 2},
+	404: {CSharp, 2},
+	381: {D, 2},
+	360: {DSharp, 2},
+	339: {E, 2},
+	320: {F, 2},
+	302: {FSharp, 2},
+	285: {G, 2},
+	269: {GSharp, 2},
+	254: {A, 2},
+	240: {ASharp, 2},
+	226: {B, 2},
+
+	214: {C, 3},
+	202: {CSharp, 3},
+	190: {D, 3},
+	180: {DSharp, 3},
+	170: {E, 3},
+	160: {F, 3},
+	151: {FSharp, 3},
+	143: {G, 3},
+	135: {GSharp, 3},
+	127: {A, 3},
+	120: {ASharp, 3},
+	113: {B, 3},
+
+	107: {C, 4},
+	101: {CSharp, 4},
+	95:  {D, 4},
+	90:  {DSharp, 4},
+	85:  {E, 4},
+	80:  {F, 4},
+	76:  {FSharp, 4},
+	71:  {G, 4},
+	67:  {GSharp, 4},
+	64:  {A, 4},
+	60:  {ASharp, 4},
+	57:  {B, 4},
+}
 
 type rawModule struct {
 	Name          [20]byte
@@ -25,6 +106,37 @@ type rawModule struct {
 
 func (s rawModule) getName() string {
 	return string(bytes.TrimRight(s.Name[:], "\x00"))
+}
+
+type noteRaw uint32
+
+func (n noteRaw) ToNote() (note NoteData) {
+	sup := uint32(0xF0000000) & uint32(n) >> 28
+	per := uint32(0x0FFF0000) & uint32(n) >> 16
+	slo := uint32(0x0000F000) & uint32(n) >> 12
+	eff := uint32(0x00000F00) & uint32(n) >> 8
+	efp := uint32(0x000000FF) & uint32(n)
+
+	// Combine sample upper and lower bits
+	sample := uint8(sup<<4 | slo)
+
+	var info *NoteInfo
+	ni, ok := PeriodMap[uint16(per)]
+	if ok {
+		info = &ni
+	} else {
+		if per > 0 {
+			log.Fatalf("Period not found %v.", per)
+		}
+		info = nil
+	}
+
+	return NoteData{
+		SampleNumber: sample,
+		Note:         info,
+		Effect:       uint8(eff),
+		EffectParams: uint8(efp),
+	}
 }
 
 type rawSample struct {
@@ -48,21 +160,82 @@ type Module struct {
 
 type Pattern struct {
 	Index int
-	Data  []PatternData
+	Notes []NoteData
 }
 
-type PatternData struct {
+type NoteData struct {
 	SampleNumber uint8
-	Note         NoteData
+	Note         *NoteInfo
 	Effect       uint8
 	EffectParams uint8
 	Channel      uint8
 	Row          uint8
 }
 
-type NoteData struct {
-	Note   uint8
+func (nd NoteData) ToString() string {
+	if nd.Note == nil {
+		return fmt.Sprintf("  _  %02X %01X%02X ", nd.SampleNumber,
+			nd.Effect, nd.EffectParams)
+	}
+	return fmt.Sprintf(" %s %02X %01X%02X ", nd.Note.ToString(),
+		nd.SampleNumber, nd.Effect, nd.EffectParams)
+}
+
+type NoteInfo struct {
+	Note   NoteName
 	Octave uint8
+}
+
+func (ni NoteInfo) ToString() string {
+	return fmt.Sprintf("%s%d", ni.Note.ToString(), ni.Octave)
+}
+
+type NoteName uint8
+
+const (
+	C NoteName = iota
+	CSharp
+	D
+	DSharp
+	E
+	F
+	FSharp
+	G
+	GSharp
+	A
+	ASharp
+	B
+)
+
+func (n NoteName) ToString() string {
+	switch n {
+	case C:
+		return "C-"
+	case CSharp:
+		return "C#"
+	case D:
+		return "D-"
+	case DSharp:
+		return "D#"
+	case E:
+		return "E-"
+	case F:
+		return "F-"
+	case FSharp:
+		return "F#"
+	case G:
+		return "G-"
+	case GSharp:
+		return "G#"
+	case A:
+		return "A-"
+	case ASharp:
+		return "A#"
+	case B:
+		return "B-"
+	default:
+		return "er"
+	}
 }
 
 type Sample struct {
@@ -75,7 +248,13 @@ type Sample struct {
 }
 
 func (m Module) exportReport(baseName string) (err error) {
-	t, err := template.New("export").Parse(moduleReportTemplate)
+	funcMap := template.FuncMap{
+		"noteNewLine": func(n NoteData) bool { return n.Channel == 0 },
+		"noteToString": func(n NoteData) string {
+			return n.ToString()
+		},
+	}
+	t, err := template.New("export").Funcs(funcMap).Parse(moduleReportTemplate)
 	if err != nil {
 		return
 	}
@@ -88,6 +267,31 @@ func (m Module) exportReport(baseName string) (err error) {
 	return
 }
 
+func readPatterns(highestPattern uint8, r io.ReadSeeker) (patterns []Pattern, err error) {
+	_, err = r.Seek(1084, 0)
+	if err != nil {
+		return nil, err
+	}
+	for i := uint8(0); i < highestPattern; i++ {
+		var notes []NoteData
+		for patRow := uint8(0); patRow < 64; patRow++ {
+			notesData := make([]noteRaw, 4)
+			err = binary.Read(r, binary.BigEndian, &notesData)
+			if err != nil {
+				return nil, err
+			}
+			for idx, nd := range notesData {
+				note := nd.ToNote()
+				note.Channel = uint8(idx)
+				note.Row = patRow
+				notes = append(notes, note)
+			}
+		}
+		patterns = append(patterns, Pattern{Notes: notes, Index: int(i)})
+	}
+	return patterns, err
+}
+
 func modFromReader(r io.ReadSeeker) (mod Module, err error) {
 	var rawMod rawModule
 
@@ -96,8 +300,24 @@ func modFromReader(r io.ReadSeeker) (mod Module, err error) {
 		return mod, err
 	}
 
-	// TODO Implement other stuff
 	mod.Name = rawMod.getName()
+	mod.Type = string(rawMod.Magic[:])
+	mod.PatternsOrder = rawMod.PatternsOrder[:]
+
+	highestPattern := uint8(0)
+
+	for _, i := range rawMod.PatternsOrder {
+		if i > highestPattern {
+			highestPattern = i
+		}
+	}
+
+	patterns, err := readPatterns(highestPattern, r)
+	if err != nil {
+		log.Fatal(err)
+	}
+	mod.PatternsData = patterns
+
 	return mod, nil
 }
 
