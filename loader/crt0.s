624fec3f6b4fb998568752209c2b4edf0c7ac660
Krystian Bacławski <krystian.baclawski@gmail.com>
2020-08-14 12:10:00 +0200

    Use linker script to provide SysBase symbol.

--- loader/crt0.s
+++ loader/crt0.s
@@ -10,10 +10,7 @@
 
                 section '.text', code
 
-_start:         move.l  $4.w,a6
-                move.l  a6,_SysBase
-
-                lea     ___INIT_LIST__,a0
+_start:         lea     ___INIT_LIST__,a0
                 jsr     _CallFuncList
 
                 jsr     _main
@@ -28,9 +25,5 @@ _exit:          lea     ___EXIT_LIST__,a0
 ; one source for both code models
 
 _geta4:         rts
-
-                section '.bss', bss
-
-_SysBase:       ds.l    1
 
 ; vim: ft=asm68k:ts=8:sw=8
