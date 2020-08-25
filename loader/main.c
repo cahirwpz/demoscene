05d5a091b8a7940d2a2b5a29e237e0e6b6813298
Krystian Bacławski <krystian.baclawski@gmail.com>
2020-08-25 12:28:24 +0200

    Add implementation of TaskVBlankWait

--- loader/main.c
+++ loader/main.c
@@ -1,20 +1,41 @@
 #include <custom.h>
+#include <interrupt.h>
+#include <task.h>
 
 #include "sync.h"
 #include "effect.h"
 
 extern EffectT Effect;
 
-/* TODO Puts a task into sleep waiting for VBlank interrupt. */
+static u_char IsWaiting = 0;
+
+static void VBlankWakeupHandler(void) {
+  if (IsWaiting) {
+    IsWaiting = 0;
+    TaskNotifyISR(INTF_VERTB);
+  }
+}
+
+INTSERVER(VertBlankWakeup, 0, (IntFuncT)VBlankWakeupHandler, NULL);
+
+/* Puts a task into sleep waiting for VBlank interrupt. */
 void TaskWaitVBlank(void) {
-  WaitVBlank();
+  IntrDisable();
+  IsWaiting = -1;
+  TaskWait(INTF_VERTB);
+  IntrEnable();
 }
 
 int main(void) {
+  AddIntServer(VertBlankChain, VertBlankWakeup);
+
   EffectLoad(&Effect);
   EffectInit(&Effect);
   EffectRun(&Effect);
   EffectKill(&Effect);
   EffectUnLoad(&Effect);
+
+  RemIntServer(VertBlankChain, VertBlankWakeup);
+
   return 0;
 }
