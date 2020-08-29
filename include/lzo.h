88ddcee94b5bae9a7c28ea9a28786b91f042105c
Krystian Bacławski <krystian.baclawski@gmail.com>
2020-08-29 10:03:39 +0200

    We're not going to use LZO, but LZ4 instead (if at all).

--- include/lzo.h
+++
@@ -1,9 +0,0 @@
-#ifndef __LZO_H__
-#define __LZO_H__
-
-#include "types.h"
-
-int lzo1x_decompress(const u_char *in asm("a0"), u_int in_len asm("d0"),
-                      u_char *out asm("a1"), u_int *out_len asm("a2"));
-
-#endif
