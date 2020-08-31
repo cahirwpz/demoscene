2bf30e99eed7a6b50035ae9e5866ffa7251e7d81
Krystian Bacławski <krystian.baclawski@gmail.com>
2020-08-31 21:16:07 +0200

    Dump HUNK_HEADER.

--- tools/hunk/hunk_header.go
+++ tools/hunk/hunk_header.go
@@ -1,28 +1,45 @@
 package hunk
 
-import "io"
+import (
+	"fmt"
+	"io"
+	"strings"
+)
 
 type HunkHeader struct {
-	residents  []string
-	hunks      uint32
-	first      uint32
-	last       uint32
-	specifiers []uint32
+	Residents  []string
+	Hunks      uint32
+	First      uint32
+	Last       uint32
+	Specifiers []uint32
 }
 
 func readHunkHeader(r io.Reader) (h HunkHeader) {
-	h.residents = readStringArray(r)
-	h.hunks = readLong(r)
-	h.first = readLong(r)
-	h.last = readLong(r)
-	n := h.last - h.first + 1
-	h.specifiers = make([]uint32, n)
+	h.Residents = readStringArray(r)
+	h.Hunks = readLong(r)
+	h.First = readLong(r)
+	h.Last = readLong(r)
+	n := h.Last - h.First + 1
+	h.Specifiers = make([]uint32, n)
 	for i := 0; i < int(n); i++ {
-		h.specifiers[i] = readLong(r)
+		h.Specifiers[i] = readLong(r)
 	}
 	return
 }
 
 func (h HunkHeader) String() string {
-	return "HUNK_HEADER\n"
+	var sb strings.Builder
+	sb.WriteString("HUNK_HEADER\n")
+	fmt.Fprintf(&sb, "  Residents: %s\n", h.Residents)
+	for i, v := range h.Specifiers {
+		fmt.Fprintf(&sb, "  Hunk %d: %6d bytes", i+int(h.First), v<<2)
+		if v&HUNKF_CHIP != 0 {
+			sb.WriteString(" [MEMF_CHIP]\n")
+		} else if v&HUNKF_FAST != 0 {
+			sb.WriteString(" [MEMF_FAST]\n")
+		} else {
+			sb.WriteString(" [MEMF_PUBLIC]\n")
+		}
+	}
+	return sb.String()
 }
