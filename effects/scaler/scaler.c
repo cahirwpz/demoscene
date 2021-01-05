51473bb19c4c7c7840f1239b5e1b5a3a2381db66
Krystian Bacławski <krystian.baclawski@gmail.com>
2021-01-05 12:11:26 +0100

    Vertical scaling - first attempt.

---
+++ effects/scaler/scaler.c
@@ -0,0 +1,80 @@
+#include "effect.h"
+#include "custom.h"
+#include "copper.h"
+#include "bitmap.h"
+#include "palette.h"
+#include "profiler.h"
+
+#include "data/stars-die.c"
+
+#define LINES 256
+
+static CopListT *cp0, *cp1;
+
+/* static */ void MakeCopperList(CopListT *cp, short height) {
+  short ys = (LINES - height) / 2;
+  int dy = (LINES << 16) / height;
+
+  CopInit(cp);
+  CopSetupDisplayWindow(cp, MODE_LORES, X(0), Y(ys), image.width, height);
+  CopSetupBitplanes(cp, NULL, &image, image.depth);
+
+  {
+    short n = (short)(dy >> 16) * (short)image.depth;
+    short mod = (short)image.bytesPerRow * (short)(n - 1);
+    int y = 0;
+    short i;
+
+    for (i = 1; i < height; i++) {
+      short _mod = mod;
+      int ny = y + dy;
+      if ((u_short)ny < (u_short)y)
+        _mod += image.bytesPerRow * image.depth;
+      CopWait(cp, Y(ys + i), 0);
+      CopMove16(cp, bpl1mod, _mod);
+      CopMove16(cp, bpl2mod, _mod);
+      y = ny;
+    }
+  }
+
+  CopEnd(cp);
+}
+
+/* static */ void Init(void) {
+  LoadPalette(&image_pal, 0);
+  SetupPlayfield(MODE_LORES, image.depth,
+                 X(0), Y(0), image.width, image.height);
+
+  cp0 = NewCopList(40 + LINES * 3);
+  cp1 = NewCopList(40 + LINES * 3);
+  MakeCopperList(cp0, LINES);
+  CopListActivate(cp0);
+
+  EnableDMA(DMAF_RASTER);
+}
+
+static void Kill(void) {
+  DisableDMA(DMAF_COPPER | DMAF_RASTER);
+  DeleteCopList(cp0);
+  DeleteCopList(cp1);
+}
+
+PROFILE(Scaler);
+
+static void Render(void) {
+  static short val = 0, dir = 1;
+
+  ProfilerStart(Scaler);
+  MakeCopperList(cp0, LINES - val);
+  ProfilerStop(Scaler);
+
+  CopListRun(cp0);
+  TaskWaitVBlank();
+  { CopListT *tmp = cp0; cp0 = cp1; cp1 = tmp; }
+
+  val += dir;
+  if ((val == 0) || (val == LINES - 2))
+    dir = -dir;
+}
+
+EFFECT(scaler, NULL, NULL, Init, Kill, Render);
