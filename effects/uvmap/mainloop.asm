5e2ec9fe3061f48c9342254c27aeb135028dc0ac
Krystian Bacławski <krystian.baclawski@gmail.com>
2022-01-17 21:00:41 +0100

    Count processor / bus cycles.

--- effects/uvmap/mainloop.asm
+++ effects/uvmap/mainloop.asm
@@ -1,8 +1,9 @@
         section ".text"
 
-        move.w  $1111(a1),d0   ; hi
-        or.w    $2222(a2),d0   ; lo
-        move.b  $3333(a1),d0   ; hi
-        or.b    $4444(a2),d0   ; lo
-        move.w  d0,(a0)+
-
+                                ; cpu/bus cycles
+        move.w  $1111(a1),d0    ; 12/3
+        or.w    $2222(a2),d0    ; 12/3
+        move.b  $3333(a1),d0    ; 12/3
+        or.b    $4444(a2),d0    ; 12/3
+        move.w  d0,(a0)+        ; 8/2
+                                ; 14/3.5 per pixel  
