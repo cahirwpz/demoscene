b5de2cce807710c578ceaa5f4962807e0f49e02f
Krystian Bacławski <krystian.baclawski@gmail.com>
2022-01-25 09:04:24 +0100

    Main loop optimized using addx trick

--- effects/rotator/mainloop.asm
+++ effects/rotator/mainloop.asm
@@ -2,19 +2,6 @@
 
         section ".text"
 
-getuv   macro
-        move.w  d0,d2
-        move.w  d1,d3
-        lsr.w   #7,d3
-        move.b  d3,d2
-        and.w   #$7ffe,d2
-        endm
-
-incuv   macro
-        add.w   a3,d0
-        add.w   a4,d1
-        endm
-
 ; [a0] chunky
 ; [a1] txtHi
 ; [a2] txtLo
@@ -26,65 +13,84 @@ incuv   macro
 WIDTH   equ 160
 HEIGHT  equ 100
 
+getuv   macro
+        move.w	d1,d2   ; ----VVvv
+	add.l	d4,d1
+	move.b	d0,d2	; ----VVUU
+	addx.b	d3,d0
+        add.b   d2,d2
+        and.w   #$7ffe,d2
+        endm
+
 _Rotator:
         movem.w d2-d7/a3-a6,-(sp)
 
-        move.w  d3,a3
-        move.w  d4,a4
+        clr.l   d0      ; ------UU 
+        clr.l   d1      ; uu--VVvv
+
+                        ; ----UUuu
+                        ; ----VVvv
+        lsl.l   #8,d3   ; --UUuu--
+        swap    d4      ; VVvv----
+        move.w  d3,d4   ; VVvvuu--
+        swap    d4      ; uu--VVvv [d4]
+        clr.w   d3      ; --UU----
+        swap    d3      ; ------UU [d3]
+
         move.w  d5,a5
         move.w  d6,a6
-
-        clr.w   d0
-        clr.w   d1
 
         move.w  #HEIGHT-1,d7
 .loopy
         swap    d7
-        movem.w d0-d1,-(sp)
+        movem.l d0-d1,-(sp)
+
+                        ; ----UUuu
+                        ; ----VVvv
+        lsl.l   #8,d0   ; --UUuu--
+        swap    d1      ; VVvv----
+        move.w  d0,d1   ; VVvvuu--
+        swap    d1      ; uu--VVvv [d1]
+        clr.w   d0      ; --UU----
+        swap    d0      ; ------UU [d0]
+
+        move.l	d4,d2   ; uu--VVvv
+	clr.w	d2      ; uu------
+	add.l	d2,d1   ; init X-flag
 
         move.w  #WIDTH/8-1,d7
 .loopx
         ; [a b c d e f g h] => [a b e f c d g h]
 
         getuv
-        move.w  (a1,d2.w),d4
-        incuv
-
-        getuv
-        or.w    (a2,d2.w),d4    ; [d4] hi0
-        incuv
-
-        getuv
         move.w  (a1,d2.w),d5
-        incuv
 
         getuv
-        or.w    (a2,d2.w),d5    ; [d5] lo0
-        incuv
+        or.w    (a2,d2.w),d5    ; [d5] hi0
 
         getuv
-        move.b  (a1,d2.w),d4
-        incuv
+        move.w  (a1,d2.w),d6
 
         getuv
-        or.b    (a2,d2.w),d4    ; [d4] (hi0 | hi1)
-        incuv
-
-        move.w  d4,(a0)+        ; *chunky++
+        or.w    (a2,d2.w),d6    ; [d6] lo0
 
         getuv
         move.b  (a1,d2.w),d5
-        incuv
 
         getuv
-        or.b    (a2,d2.w),d5    ; [d5] (lo0 | lo1)
-        incuv
-
+        or.b    (a2,d2.w),d5    ; [d5] (hi0 | hi1)
         move.w  d5,(a0)+        ; *chunky++
+
+        getuv
+        move.b  (a1,d2.w),d6
+
+        getuv
+        or.b    (a2,d2.w),d6    ; [d6] (lo0 | lo1)
+        move.w  d6,(a0)+        ; *chunky++
 
         dbf     d7,.loopx
 
-        movem.w (sp)+,d0-d1
+        movem.l (sp)+,d0-d1
         add.w   a5,d0
         add.w   a6,d1
         swap    d7
