0b9f7246371968db43e39c4a66e2d85452584909
Krystian Bacławski <krystian.baclawski@gmail.com>
2022-01-31 09:14:17 +0100

    Operate on 16-bit words.

--- effects/lines/CpuLineOpt.asm
+++ effects/lines/CpuLineOpt.asm
@@ -23,12 +23,13 @@ _CpuLineOpt:
 
         move.w  d0,d5
         lsr.w   #3,d5
-        add.w   d5,a0           ; pixels += xs >> 3
+        and.w   #-2,d5
+        add.w   d5,a0           ; pixels += (xs >> 3) & -2
 
         move.w  d0,d5
-        andi.w  #7,d5
-        moveq   #-128,d4
-        lsr.b   d5,d4           ; [d4] color = 0x80 >> (xs & 7)
+        andi.w  #15,d5
+        move.w  #$8000,d4
+        lsr.w   d5,d4           ; [d4] color = 0x8000 >> (xs & 15)
 
         sub.w   d0,d2           ; [d2] dx = abs(xe - xs)
         move.w  d2,d0           ; [d0] xe - xs
@@ -57,41 +58,41 @@ dx_dy:  tst.w   d3              ; dy == 0 ?
 
 case1:
 
-.loop   or.b    d4,(a0)         ; *pixels |= color
+.loop   or.w    d4,(a0)         ; *pixels |= color
         add.w   a1,a0           ; pixels += stride
 
         tst.w   d1
         blt.s   .else           ; dg > 0
 
-        rol.b   #1,d4
+        rol.w   #1,d4
         bcc.s   .skip
-        subq.l  #1,a0
+        subq.l  #2,a0
 
-.skip   add.w   d6,d1
+.skip   add.w   d6,d1           ; dg += dg1
         dbf     d3,.loop
         bra.s   exit
 
-.else   add.w   d5,d1
+.else   add.w   d5,d1           ; dg += dg2
         dbf     d3,.loop
         bra.s   exit
 
 case2:
 
-.loop   or.b    d4,(a0)         ; *pixels |= color
+.loop   or.w    d4,(a0)         ; *pixels |= color
         add.w   a1,a0           ; pixels += stride
 
         tst.w   d1
         blt.s   .else           ; dg > 0
 
-        ror.b   #1,d4
+        ror.w   #1,d4
         bcc.s   .skip
-        addq.l  #1,a0
+        addq.l  #2,a0
 
-.skip   add.w   d6,d1
+.skip   add.w   d6,d1           ; dg += dg2
         dbf     d3,.loop
         bra.s   exit
 
-.else   add.w   d5,d1
+.else   add.w   d5,d1           ; dg += dg1
         dbf     d3,.loop
         bra.s   exit
 
@@ -112,41 +113,41 @@ dy_dx:  tst.w   d2              ; dx == 0 ?
 
 case3:
 
-.loop   or.b    d4,(a0)         ; *pixels |= color
+.loop   or.w    d4,(a0)         ; *pixels |= color
 
-        rol.b   #1,d4
+        rol.w   #1,d4
         bcc.s   .skip
-        subq.l  #1,a0
+        subq.l  #2,a0
 
 .skip   tst.w   d1
         blt.s   .else           ; dg > 0
 
         add.w   a1,a0           ; pixels += stride
-        add.w   d6,d1
+        add.w   d6,d1           ; dg += dg1
         dbf     d2,.loop
         bra.s   exit
 
-.else   add.w   d5,d1
+.else   add.w   d5,d1           ; dg += dg2
         dbf     d2,.loop
         bra.s   exit
 
 case4:
 
-.loop   or.b    d4,(a0)         ; *pixels |= color
+.loop   or.w    d4,(a0)         ; *pixels |= color
 
-        ror.b   #1,d4
+        ror.w   #1,d4
         bcc.s   .skip
-        addq.l  #1,a0
+        addq.l  #2,a0
 
 .skip   tst.w   d1
         blt.s   .else           ; dg > 0
 
         add.w   a1,a0           ; pixels += stride
-        add.w   d6,d1
+        add.w   d6,d1           ; dg += dg1
         dbf     d2,.loop
         bra.s   exit
 
-.else   add.w   d5,d1
+.else   add.w   d5,d1           ; dg += dg2
         dbf     d2,.loop
 
 exit:   movem.l (sp)+,d2-d6
