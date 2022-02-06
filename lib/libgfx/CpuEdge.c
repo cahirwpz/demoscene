64c0fe6bdfffd14dd42b78963941740dc5b2220f
Krystian Bacławski <krystian.baclawski@gmail.com>
2022-02-06 15:12:20 +0100

    Speed up line & edge drawing using CPU (#73)
    * fixed buggy CpuEdge

--- lib/libgfx/CpuEdge.c
+++ lib/libgfx/CpuEdge.c
@@ -10,86 +10,83 @@ void CpuEdgeSetup(const BitmapT *bitmap, u_short plane) {
   edge.stride = bitmap->bytesPerRow;
 }
 
-void CpuEdge(short x1 asm("d0"), short y1 asm("d1"), short x2 asm("d2"), short y2 asm("d3")) {
+void CpuEdge(short xs asm("d0"), short ys asm("d1"),
+             short xe asm("d2"), short ye asm("d3"))
+{
   register u_char *pixels asm("a0") = edge.pixels;
-  register int stride asm("a1") = edge.stride;
+  short stride = edge.stride;
+  short dx, dy, di, df, n, xi, xf;
+  int adx;
 
-  if (y1 == y2)
+  if (ys > ye) {
+    swapr(xs, xe);
+    swapr(ys, ye);
+  }
+
+  dx = xe - xs;
+  dy = ye - ys;
+
+  if (dy == 0)
     return;
 
-  {
-    short dy = y2 - y1;
-    short dx = x2 - x1;
-    u_char color = 0x80 >> (x1 & 7);
+  pixels += ys * stride;
+  pixels += xs >> 3;
 
-    pixels += (int)((short)y1 * (short)stride);
-    pixels += x1 >> 3;
+  adx = absw(dx);
 
-    if (dx < 0)
-      dx = -dx;
+  if (adx < dy) {
+    di = 0;
+    df = adx;
+  } else {
+    divmod16(adx, dy, df, di);
+  }
 
-    if (dx <= dy) {
-      short err = dy / 2;
-      short n = dy - 1;
+  xi = ~xs & 7;
+  xf = -dy;
+  n = dy - 1;
 
-      if (x1 <= x2) {
-        do {
-          *pixels ^= color;
-          pixels += stride;
-          err += dx;
-          if (err >= dy) {
-            asm("rorb  #1,%0\n"
-                "bccs  . +4\n"
-                "addql #1,%1\n"
-                : "+d" (color), "+a" (pixels));
-            err -= dy;
-          }
-        } while (--n != -1);
-      } else {
-        do {
-          *pixels ^= color;
-          pixels += stride;
-          err += dx;
-          if (err >= dy) {
-            asm("rolb  #1,%0\n"
-                "bccs  . +4\n"
-                "subql #1,%1\n"
-                : "+d" (color), "+a" (pixels));
-            err -= dy;
-          }
-        } while (--n != -1);
-      } 
-    } else {
-      short err = dx / 2; /* Makes line symmetric on both ends... I guess */
-      short n = dx - 1;
+  if (dx >= 0) {
+    di = di & 7;
+    stride += di >> 3;
 
-      if (x1 <= x2) {
-        do {
-          err += dy;
-          if (err >= dx) {
-            *pixels ^= color;
-            pixels += stride;
-            err -= dx;
-          }
-          asm("rorb  #1,%0\n"
-              "bccs  . +4\n"
-              "addql #1,%1\n"
-              : "+d" (color), "+a" (pixels));
-        } while (--n != -1);
-      } else {
-        do {
-          err += dy;
-          if (err >= dx) {
-            *pixels ^= color;
-            pixels += stride;
-            err -= dx;
-          }
-          asm("rolb  #1,%0\n"
-              "bccs  . +4\n"
-              "subql #1,%1\n"
-              : "+d" (color), "+a" (pixels));
-        } while (--n != -1);
+    do {
+      bchg(pixels, xi);
+
+      pixels += stride;
+
+      xf += df;
+      if (xf >= 0) {
+        xi--;
+        xf -= dy;
       }
-    }
+
+      xi -= di;
+      if (xi < 0) {
+        pixels++;
+        xi += 8;
+      }
+    } while (--n != -1);
+  } else {
+    stride -= di >> 3;
+    di = -(di & 7);
+    xi -= 8;
+
+    do {
+      bchg(pixels, xi);
+
+      pixels += stride;
+
+      xf += df;
+      if (xf >= 0) {
+        xi++;
+        xf -= dy;
+      }
+
+      xi -= di;
+      if (xi >= 0) {
+        pixels--;
+        xi -= 8;
+      }
+    } while (--n != -1);
   }
 }
--- lib/libgfx/CpuLine.c
+++ lib/libgfx/CpuLine.c
@@ -13,9 +13,9 @@ void CpuLineSetup(const BitmapT *bitmap, u_short plane) {
 void CpuLine(short xs asm("d0"), short ys asm("d1"),
              short xe asm("d2"), short ye asm("d3"))
 {
-  register u_char *pixels asm("a0") = line.pixels;
-  register int stride asm("a1") = line.stride;
-  u_char color;
+  register void *pixels asm("a0") = line.pixels;
+  short stride = line.stride;
+  u_short color;
   short dx, dy;
 
   if (ys > ye) {
@@ -23,90 +23,96 @@ void CpuLine(short xs asm("d0"), short ys asm("d1"),
     swapr(ys, ye);
   }
 
-  dx = abs(xe - xs);
+  pixels += ys * stride;
+  pixels += (xs >> 3) & -2;
+
+  color = 0x8000 >> (xs & 15);
+
   dy = ye - ys;
 
-  pixels += ys * (short)stride;
-  pixels += (int)xs >> 3;
+  if (dy == 0)
+    return;
 
-  color = 0x80 >> (xs & 7);
+  dx = abs(xe - xs);
 
   if (dx < dy) {
     short dg2 = 2 * dx;
     short dg = dg2 - dy;
-    short dg1 = dg - dy;
+    short dg1 = 2 * dy;
 
-    if (dy == 0)
-      return;
+    dg -= dg2; /* precompensate for [dg += dg2] */
+    dy--;
 
     if (xe < xs) {
+      /* case 1 */
       do {
-        *pixels |= color;
+        *(u_short *)pixels |= color;
+
+        dg += dg2;
+
+        if (dg >= 0) {
+          dg -= dg1;
+          asm("rolw  #1,%0\n"
+              "bccs  . +4\n"
+              "subql #2,%1\n" : "+r" (color), "+r" (pixels));
+        }
 
         pixels += stride;
-
-        if (dg > 0) {
-          asm("rolb  #1,%0\n"
-              "bccs  . +4\n"
-              "subql #1,%1\n" : "+r" (color), "+r" (pixels));
-          dg += dg1;
-        } else {
-          dg += dg2;
-        }
       } while (--dy != -1);
     } else {
+      /* case 2 */
       do {
-        *pixels |= color;
+        *(u_short *)pixels |= color;
+
+        dg += dg2;
+
+        if (dg >= 0) {
+          asm("rorw  #1,%0\n"
+              "bccs  . +4\n"
+              "addql #2,%1\n" : "+r" (color), "+r" (pixels));
+          dg -= dg1;
+        }
 
         pixels += stride;
-
-        if (dg > 0) {
-          asm("rorb  #1,%0\n"
-              "bccs  . +4\n"
-              "addql #1,%1\n" : "+r" (color), "+r" (pixels));
-          dg += dg1;
-        } else {
-          dg += dg2;
-        }
       } while (--dy != -1);
     }
   } else {
     short dg2 = 2 * dy;
     short dg = dg2 - dx;
-    short dg1 = dg - dx;
+    short dg1 = 2 * dx;
 
-    if (dx == 0)
-      return;
+    dg -= dg2; /* precompensate for [dg += dg2] */
+    dy--;
 
     if (xe < xs) {
+      /* case 3 */
       do {
-        *pixels |= color;
+        *(u_short *)pixels |= color;
 
-        asm("rolb  #1,%0\n"
-            "bccs  . +4\n"
-            "subql #1,%1\n" : "+r" (color), "+r" (pixels));
-
-        if (dg > 0) {
+        dg += dg2;
+        if (dg >= 0) {
+          dg -= dg1;
           pixels += stride;
-          dg += dg1;
-        } else {
-          dg += dg2;
         }
+
+        asm("rolw  #1,%0\n"
+            "bccs  . +4\n"
+            "subql #2,%1\n" : "+r" (color), "+r" (pixels));
       } while (--dx != -1);
     } else {
+      /* case 4 */
       do {
-        *pixels |= color;
+        *(u_short *)pixels |= color;
 
-        asm("rorb  #1,%0\n"
-            "bccs  . +4\n"
-            "addql #1,%1\n" : "+r" (color), "+r" (pixels));
-
-        if (dg > 0) {
+        dg += dg2;
+        if (dg >= 0) {
+          dg -= dg1;
           pixels += stride;
-          dg += dg1;
-        } else {
-          dg += dg2;
         }
+
+        asm("rorw  #1,%0\n"
+            "bccs  . +4\n"
+            "addql #2,%1\n" : "+r" (color), "+r" (pixels));
       } while (--dx != -1);
     }
   }
