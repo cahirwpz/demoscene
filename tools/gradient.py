9d2024b4fedb412895d05943578499bb5f2a5f5c
Krystian Bacławski <cahirwpz@cs.uni.wroc.pl>
2022-02-08 12:55:34 +0100

    Rework gradient script to accept commands list

--- tools/gradient.py
+++ tools/gradient.py
@@ -74,41 +74,54 @@ def hsv_to_rgb(c):
     return (int(r * 255.0), int(g * 255.0), int(b * 255.0))
 
 
-def getcolors(path):
+def load_pal(path):
     im = Image.open(path)
     pal = im.getpalette()
     n = max(i for _, i in im.getcolors()) + 1
     return [(pal[i * 3], pal[i * 3 + 1], pal[i * 3 + 2]) for i in range(n)]
 
 
+def copy_pal(pal):
+    return [(r, g, b) for r, g, b in pal]
+
+
 def invert(pal):
     return [(255 - r, 255 - g, 255 - b) for r, g, b in pal]
 
 
-def modify_hsv(pal, dh, ds, dv):
+def modify_hsv(pal, hue=0.0, sat=0.0, val=0.0):
     out = []
     for h, s, v in map(rgb_to_hsv, pal):
-        h = frpart(h + dh)
-        s = constrain(s + ds, 0.0, 1.0)
-        v = constrain(v + dv, 0.0, 1.0)
+        h = frpart(h + hue)
+        s = constrain(s + sat, 0.0, 1.0)
+        v = constrain(v + val, 0.0, 1.0)
         out.append(hsv_to_rgb((h, s, v)))
     return out
 
 
-def gradient(pal1, pal2, path_out):
-    n = len(pal1)
-    im = Image.new('RGB', (n, 16))
+def gradient(pals, path):
+    n = len(pals)
+    nc = len(pals[0])
+
+    im = Image.new('RGB', (nc, 15 * (n - 1) + 1))
     pix = im.load()
 
-    for y in range(16):
-        for x in range(n):
-            r = lerp(pal1[x][0], pal2[x][0], float(y) / 15)
-            g = lerp(pal1[x][1], pal2[x][1], float(y) / 15)
-            b = lerp(pal1[x][2], pal2[x][2], float(y) / 15)
+    for x in range(nc):
+        pix[x, 0] = pals[0][x]
 
-            pix[x, y] = (int(r), int(g), int(b))
+    for i in range(n - 1):
+        pal1 = pals[i]
+        pal2 = pals[i + 1]
 
-    im.save(path_out, 'PNG')
+        for y in range(15):
+            for x in range(nc):
+                r = lerp(pal1[x][0], pal2[x][0], float(y) / 15)
+                g = lerp(pal1[x][1], pal2[x][1], float(y) / 15)
+                b = lerp(pal1[x][2], pal2[x][2], float(y) / 15)
+
+                pix[x, 1 + y + i * 15] = (int(r), int(g), int(b))
+
+    im.save(path, 'PNG')
 
 
 if __name__ == '__main__':
@@ -116,43 +129,63 @@ if __name__ == '__main__':
         description='Generate color gradient from given source pallete. '
                     'Destination palette may be delivered from source one, '
                     'otherwise is taken from source and modified.')
-    parser.add_argument('--palette', type=str,
-                        help='Take destination palette from file.')
-    parser.add_argument('--invert', action='store_true',
-                        help='Invert colors (aka negative).')
-    parser.add_argument('--hue', type=float, default=0.0,
-                        help='Change Hue component in HSV space. The value '
-                             'wraps around in [0.0, 1.0] range.')
-    parser.add_argument('--saturation', type=float, default=0.0,
-                        help='Change Saturation component in HSV space. The '
-                             'value is constrained within [0.0, 1.0] range.')
-    parser.add_argument('--value', type=float, default=0.0,
-                        help='Change Value component in HSV space. The '
-                             'value is constrained within [0.0, 1.0] range.')
-    parser.add_argument('input', metavar='INPUT', type=str,
-                        help='Image from which source palette will be '
-                             'extracted.')
-    parser.add_argument('output', metavar='OUTPUT', type=str,
-                        help='Output gradient file. Image of N x 16 size '
-                             'in RGB space. Each line represents single '
-                             'palette. There are 14 transitions between '
-                             'source and destination palette.')
+    parser.add_argument('cmds', nargs='+')
+    # parser.add_argument('--palette', type=str,
+    #                     help='Take destination palette from file.')
+    # parser.add_argument('--invert', action='store_true',
+    #                     help='Invert colors (aka negative).')
+    # parser.add_argument('--hue', type=float, default=0.0,
+    #                     help='Change Hue component in HSV space. The value '
+    #                          'wraps around in [0.0, 1.0] range.')
+    # parser.add_argument('--saturation', type=float, default=0.0,
+    #                     help='Change Saturation component in HSV space. The '
+    #                          'value is constrained within [0.0, 1.0] range.')
+    # parser.add_argument('--value', type=float, default=0.0,
+    #                     help='Change Value component in HSV space. The '
+    #                          'value is constrained within [0.0, 1.0] range.')
+    # parser.add_argument('input', metavar='INPUT', type=str,
+    #                     help='Image from which source palette will be '
+    #                          'extracted.')
+    # parser.add_argument('output', metavar='OUTPUT', type=str,
+    #                     help='Output gradient file. Image of N x 16 size '
+    #                          'in RGB space. Each line represents single '
+    #                          'palette. There are 14 transitions between '
+    #                          'source and destination palette.')
     args = parser.parse_args()
+    cmds = args.cmds
 
-    if not os.path.isfile(args.input):
-        raise SystemExit('Input file does not exists!')
+    pals = []
 
-    if args.palette and not os.path.isfile(args.palette):
-        raise SystemExit('Palette file does not exists!')
+    while cmds:
+        cmd = cmds.pop(0)
 
-    pal_src = getcolors(args.input)
-    if args.palette:
-        pal_dst = getcolors(args.palette)
-    if args.invert:
-        pal_dst = invert(pal_src)
-    if args.hue or args.saturation or args.value:
-        pal_dst = modify_hsv(pal_src, args.hue, args.saturation, args.value)
+        if cmd == 'load':
+            try:
+                path = cmds.pop(0)
+            except ValueError:
+                raise SystemExit('load: file path expected!')
 
-    assert len(pal_src) == len(pal_dst)
+            if not os.path.isfile(path):
+                raise SystemExit(f'{path}: file does not exists!')
 
-    gradient(pal_src, pal_dst, args.output)
+            pals.append(load_pal(path))
+        elif cmd == 'save':
+            try:
+                path = cmds.pop(0)
+            except ValueError:
+                raise SystemExit('save: file path expected!')
+
+            gradient(pals, path)
+            break
+        elif cmd == 'copy':
+            pals.append(copy_pal(pals[-1]))
+        elif cmd == 'neg':
+            pals[-1] = invert(pals[-1])
+        elif cmd in ['hue', 'sat', 'val']:
+            try:
+                v = float(cmds.pop(0))
+            except (ValueError, TypeError):
+                raise SystemExit(f'{cmd}: floating point number expected!')
+
+            kwargs = {cmd: v}
+            pals[-1] = modify_hsv(pals[-1], **kwargs)
