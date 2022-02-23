18880a50d0985e51b78b3fdfcb295a222e1cd42c
Kristopher38 <kristopher38@wp.pl>
2022-02-23 15:24:40 +0100

    Fix python formatting

--- effects/game-of-life/rle2png.py
+++ effects/game-of-life/rle2png.py
@@ -1,31 +1,37 @@
 from sys import argv
 from PIL import Image
 import re
+ 
+header_pattern = re.compile(
+    r'x\s*=\s*(\d+)\s*,\s*y\s*=\s*(\d+)(?:\s*,\s*rule\s*=\s*(\w+\/\w+)\s*)*'
+)
+rle_pattern = re.compile(r'(\d*)(o|b|\$)')
 
 with open(argv[1], 'r') as f:
     # skip comments
     line = f.readline().strip()
     while line.startswith("#"):
         line = f.readline().strip()
-    
+
     if line == '':
         line = f.readline().strip()
     header = line
 
     # read header
-    header_pattern = re.compile(r'x\s*=\s*(\d+)\s*,\s*y\s*=\s*(\d+)(?:\s*,\s*rule\s*=\s*(\w+\/\w+)\s*)*')
     captures = header_pattern.match(header)
     x = int(captures.group(1))
     y = int(captures.group(2))
     rule = captures.group(3)
 
     # decode RLE into the image
-    rle_pattern = re.compile(r'(\d*)(o|b|\$)') 
-    im = Image.new("P", (x, y)) # pallete mode (only this can produce 1-bit image, 1 and L seem to always output 8-bit)
+    # pallete mode (only this can produce 1-bit image,
+    # 1 and L seem to always output 8-bit)
+    im = Image.new("P", (x, y))
     cur_x = 0
     cur_y = 0
     for line in f:
-        captures = [(int(count) if count != '' else 1, tag) for count, tag in rle_pattern.findall(line)]
+        captures = [(int(count) if count != '' else 1, tag)
+                    for count, tag in rle_pattern.findall(line)]
         for count, tag in captures:
             for i in range(0, count):
                 if tag == 'b':
