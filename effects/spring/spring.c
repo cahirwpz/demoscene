7f91390d0225827246680869579bb6676c1441f8
Krystian Bacławski <cahirwpz@cs.uni.wroc.pl>
2022-02-25 17:58:22 +0100

    Format code, 16-bit mul & div, profiler stats.

--- effects/spring/spring.c
+++ effects/spring/spring.c
@@ -1,38 +1,39 @@
 #include "effect.h"
 
-#include "stdio.h"
-#include "custom.h"
-#include "copper.h"
-#include "blitter.h"
 #include "bitmap.h"
-#include "sprite.h"
+#include "blitter.h"
+#include "copper.h"
+#include "custom.h"
 #include "fx.h"
 #include "gfx.h"
+#include "sprite.h"
+#include "stdio.h"
 
 #define WIDTH 320
 #define HEIGHT 256
-#define DEPTH 4 
+#define DEPTH 4
 
-#define X_OFFSET 56 
+#define X_OFFSET 56
 
-#define AMPL 32  
+#define AMPL 32
 #define Y0 130
-#define START_DRAW 48 
+#define START_DRAW 48
 
 #include "data/background.c"
 #include "data/spiders.c"
+
 static CopListT *cp;
 static CopInsT *sprptr[8];
 
 /* Palette and references for spider sprites. */
-static PaletteT sprite_pal = {
-  .count = 15,
-  .colors = {0x0b1, 0xb4f, 0x10b, 0x0, 0x04b, 0xb70, 0x09b, 0x0, 0xbb0, 0xb01, 0x0b3, 0x0, 0x04b, 0xb70, 0x09b} 
-};
+static PaletteT sprite_pal = {.count = 15,
+                              .colors = {0x0b1, 0xb4f, 0x10b, 0x0, 0x04b, 0xb70,
+                                         0x09b, 0x0, 0xbb0, 0xb01, 0x0b3, 0x0,
+                                         0x04b, 0xb70, 0x09b}};
 
 static SpriteT *sprite[] = {
-  &spiders_sprite0, &spiders_sprite1, &spiders_sprite2, &spiders_sprite3, &spiders_sprite4, &spiders_sprite5, &spiders_sprite6, &spiders_sprite7
-};
+    &spiders_sprite0, &spiders_sprite1, &spiders_sprite2, &spiders_sprite3,
+    &spiders_sprite4, &spiders_sprite5, &spiders_sprite6, &spiders_sprite7};
 
 /* Offsets for diverging spiders movement. */
 static short factors[8] = {-8, 32, -16, 24, -24, 16, -32, 8};
@@ -40,51 +41,50 @@ static short factors[8] = {-8, 32, -16, 24, -24, 16, -32, 8};
 /* Draws lines with blitter to create a spring.
  * It takes position of a spider, numbers of points in a spring
  * and distance between each 2 in the line. */
-static void DrawSpring(int y, int x, int pts_num, int pts_dist)
-{
+static void DrawSpring(int y, int x, short pts_num, short pts_dist) {
   short x0, x1;
   short y0, y1;
-  int temp, len;
-  int delta_x, delta_y;
-  short i; 
+  short temp;
+  int len;
+  short delta_x, delta_y;
+  short i;
 
-  /* Length od the spring. */ 
+  /* Length od the spring. */
   len = y - START_DRAW;
 
-  /* Calculate vertical delta_y between points and horizontal offset from center. */ 
-  delta_y = (len)/(pts_num << 1);
+  /* Calculate vertical delta_y between points and horizontal offset from
+   * center. */
+  delta_y = div16(len, pts_num << 1);
   delta_x = ((pts_dist * pts_dist) - (delta_y * delta_y)) >> 2;
-  if(delta_x > 0)
+  if (delta_x > 0)
     delta_x = isqrt(delta_x);
   else
     delta_x = 0;
-  
+
   x0 = x + delta_x;
   x1 = x - delta_x;
-  
+
   y0 = START_DRAW;
   y1 = y0 + delta_y;
 
   BlitterLineSetup(&background_bmp, 0, LINE_SOLID);
   BlitterLine(x, y0, x0, y1);
 
-  for(i = 0; i < pts_num - 1; i++)
-  {
+  for (i = 0; i < pts_num - 1; i++) {
     temp = x0;
     x0 = x1;
     x1 = temp;
     y0 = y1;
-    y1 = y0 + 2*delta_y;
+    y1 = y0 + 2 * delta_y;
     BlitterLine(x1, y0, x0, y1);
   }
   y0 = y1;
   y1 = y0 + delta_y;
   BlitterLine(x0, y0, x, y1);
-  BlitterLine(x, y1, x, y); 
+  BlitterLine(x, y1, x, y);
 }
 
-static void MakeCopperList(CopListT *cp)
-{
+static void MakeCopperList(CopListT *cp) {
   CopInit(cp);
   CopSetupSprites(cp, sprptr);
   ITER(i, 0, 8, CopInsSet32(sprptr[i], sprite[i]));
@@ -103,35 +103,39 @@ static void MakeCopperList(CopListT *cp)
 
 static void Init(void) {
   SetupPlayfield(MODE_LORES, DEPTH, X(0), Y(0), WIDTH, HEIGHT);
-  
+
   cp = NewCopList(200);
   MakeCopperList(cp);
   CopListActivate(cp);
   CopListRun(cp);
-  
+
   EnableDMA(DMAF_RASTER | DMAF_BLITTER | DMAF_SPRITE);
 }
 
 static void Kill(void) {
-  DisableDMA(DMAF_BLITTER|DMAF_RASTER|DMAF_SPRITE);
+  DisableDMA(DMAF_BLITTER | DMAF_RASTER | DMAF_SPRITE);
   DeleteCopList(cp);
 }
 
+PROFILE(Spring);
+
 static void Render(void) {
-  short i, y_offset, k; 
+  short i, y_offset, k;
   /* Draw one half of springs and sprites every second frame. */
   k = (frameCount & 1) ? 3 : 0;
-  for(i = 0 + k; i < 3 + k; i++)
-  {
+
+  ProfilerStart(Spring);
+  for (i = 0 + k; i < 3 + k; i++) {
     y_offset = normfx(AMPL * COS(frameCount * 64 + 6 * factors[i]));
-    SpriteUpdatePos(sprite[i], 16, X(X_OFFSET*i), Y(Y0 + y_offset));
+    SpriteUpdatePos(sprite[i], 16, X(X_OFFSET * i), Y(Y0 + y_offset));
     {
       Area2D area = {.x = X_OFFSET * i + 1, .y = START_DRAW, .w = 16, .h = 130};
       BlitterClearArea(&background_bmp, 0, &area);
     }
     DrawSpring(Y0 + y_offset, X_OFFSET * i + 9, 6, 8);
-  } 
-  CopListRun(cp);
+  }
+  ProfilerStop(Spring);
+
   TaskWaitVBlank();
 }
 
