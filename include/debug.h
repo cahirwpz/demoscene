2ade341349a0d90f89744f32fa6c6bb473bea6a8
Krystian Bacławski <krystian.baclawski@gmail.com>
2022-04-24 15:45:02 +0200

    Split debug.h into system dependant / independant parts

--- include/debug.h
+++ include/debug.h
@@ -1,1 +1,25 @@
-system/debug.h
+#ifndef __DEBUG_H__
+#define __DEBUG_H__
+
+/* When simulator is configured to enter debugger on illegal instructions,
+ * this macro can be used to set breakpoints in your code. */
+#define BREAK() { asm volatile("\tillegal\n"); }
+
+/* Halt the processor by masking all interrupts and waiting for NMI. */
+#define HALT() { asm volatile("\tstop\t#0x2700\n"); }
+
+/* Use whenever a program should generate a fatal error. This will break into
+ * debugger for program inspection and stop instruction execution. */
+#define PANIC() { BREAK(); HALT(); }
+
+#include <system/debug.h>
+
+#ifdef NDEBUG
+#define Assert(e) {}
+#else
+#define Assert(e) { if (!(e))                                                  \
+   Panic("Assertion \"%s\" failed: file \"%s\", line %d, function \"%s\"!\n",  \
+         #e, __FILE__, __LINE__, __func__); }
+#endif
+
+#endif 
--- include/system/debug.h
+++ include/system/debug.h
@@ -1,18 +1,7 @@
-#ifndef __DEBUG_H__
-#define __DEBUG_H__
+#ifndef __SYSTEM_DEBUG_H__
+#define __SYSTEM_DEBUG_H__
 
 #include <cdefs.h>
-
-/* When simulator is configured to enter debugger on illegal instructions,
- * this macro can be used to set breakpoints in your code. */
-#define BREAK() { asm volatile("\tillegal\n"); }
-
-/* Halt the processor by masking all interrupts and waiting for NMI. */
-#define HALT() { asm volatile("\tstop\t#0x2700\n"); }
-
-/* Use whenever a program should generate a fatal error. This will break into
- * debugger for program inspection and stop instruction execution. */
-#define PANIC() { BREAK(); HALT(); }
 
 #ifdef UAE
 #include <system/uae.h>
@@ -25,12 +14,4 @@ __noreturn void Panic(const char *format, ...)
   __attribute__ ((format (printf, 1, 2)));
 #endif
 
-#ifdef NDEBUG
-#define Assert(e) {}
-#else
-#define Assert(e) { if (!(e))                                                  \
-   Panic("Assertion \"%s\" failed: file \"%s\", line %d, function \"%s\"!\n",  \
-         #e, __FILE__, __LINE__, __func__); }
-#endif
-
-#endif 
+#endif /* !__SYSTEM_DEBUG__ */
