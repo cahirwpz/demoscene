68d84f352be30367c4177b6887db074ec0dea132
Krystian Bacławski <krystian.baclawski@gmail.com>
2022-05-07 18:48:37 +0200

    Devise macros for creatin system call invocations.

--- include/system/memory.h
+++ include/system/memory.h
@@ -27,35 +27,11 @@ void *MemResize(void *memoryBlock, u_int byteSize);
 void MemFree(void *memoryBlock);
 void AddMemory(void *ptr, u_int byteSize, u_int attributes);
 #else
-static inline void *MemAlloc(u_int byteSize, u_int attributes) {
-  register u_int _d0 asm("d0") = byteSize;
-  register u_int _d1 asm("d1") = attributes;
-  void *_rv;
-  asm volatile ("jsr 192:W"
-                : "=d" (_rv)
-                : "0" (_d0), "d" (_d1)
-                : "d1", "a0", "a1", "cc", "memory");
-  return _rv;
-}
+#include <system/syscall.h>
 
-static inline void *MemResize(void *memoryBlock, u_int byteSize) {
-  register void *_a0 asm("a0") = memoryBlock;
-  register u_int _d0 asm("d0") = byteSize;
-  void *_rv;
-  asm volatile ("jsr 198:W"
-                : "=d" (_rv)
-                : "0" (_d0), "a" (_a0)
-                : "d1", "a1", "cc", "memory");
-  return _rv;
-}
-
-static inline void MemFree(void *memoryBlock) {
-  register void *_a0 asm("a0") = memoryBlock;
-  asm volatile ("jsr 204:W"
-                : 
-                : "a" (_a0)
-                : "d0", "d1", "a1", "cc", "memory");
-}
+SCARG2(MemAlloc, void *, u_int, byteSize, d0, u_int, attributes, d1);
+SCARG2(MemResize, void *, void *, memoryBlock, a0, u_int, byteSize, d0)
+SCARG1NR(MemFree, void *, memoryBlock, a0);
 #endif
 
 #endif
