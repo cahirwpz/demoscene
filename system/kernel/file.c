7c881075a35fe75dd109f1c328fa4d41271a2e4b
Krystian Bacławski <krystian.baclawski@gmail.com>
2022-05-12 08:15:53 +0200

    Move File{PutChar/GetChar/Printf} to libmisc.

--- system/kernel/file.c
+++ system/kernel/file.c
@@ -1,5 +1,3 @@
-#include <stdarg.h>
-#include <stdio.h>
 #include <system/file.h>
 #include <system/errno.h>
 
@@ -9,23 +7,4 @@ int NoWrite(FileT *f __unused, const void *buf __unused, u_int nbyte __unused) {
 
 int NoSeek(FileT *f __unused, int offset __unused, int whence __unused) {
   return ENOTSUP;
-}
-
-int FileGetChar(FileT *f) {
-  char c;
-  if (FileRead(f, &c, 1) < 1)
-    return -1;
-  return c;
-}
-
-void FilePutChar(FileT *f, char c) {
-  FileWrite(f, &c, 1);
-}
-
-void FilePrintf(FileT *f, const char *fmt, ...) {
-  va_list ap;
-
-  va_start(ap, fmt);
-  kvprintf((kvprintf_fn_t *)FilePutChar, f, fmt, ap);
-  va_end(ap);
 }
