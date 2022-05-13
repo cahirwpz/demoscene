e15c8afc86b7ac04cfb1207b05721290eee22ef1
Krystian Bacławski <krystian.baclawski@gmail.com>
2022-05-13 21:21:00 +0200

    Minor cleanup

--- include/system/syscall.h
+++ include/system/syscall.h
@@ -37,16 +37,16 @@
 #ifndef _SYSTEM
 #define SYSCALL1(name, rt, t1, v1, r1)                                         \
   static inline rt name(t1 v1) {                                               \
-    register rt _rv asm("d0");                                                 \
+    register int _d0 asm("d0");                                                \
     register int _d1 asm("d1");                                                \
     register int _a0 asm("a0");                                                \
     register int _a1 asm("a1");                                                \
     register t1 _##name##_r1 asm(#r1) = v1;                                    \
     asm volatile("jsr __" #name ":W"                                           \
-                 : "=r"(_rv), "=r"(_d1), "=r"(_a0), "=r"(_a1)                  \
+                 : "=r"(_d0), "=r"(_d1), "=r"(_a0), "=r"(_a1)                  \
                  : "r"(_##name##_r1)                                           \
                  : "cc", "memory");                                            \
-    return _rv;                                                                \
+    return (rt)_d0;                                                            \
   }
 #else
 #define SYSCALL1(name, rt, t1, v1, r1) rt name(t1 v1 asm(#r1))
@@ -74,17 +74,17 @@
 #ifndef _SYSTEM
 #define SYSCALL2(name, rt, t1, v1, r1, t2, v2, r2)                             \
   static inline rt name(t1 v1, t2 v2) {                                        \
-    register rt _rv asm("d0");                                                 \
+    register int _d0 asm("d0");                                                \
     register int _d1 asm("d1");                                                \
     register int _a0 asm("a0");                                                \
     register int _a1 asm("a1");                                                \
     register t1 _##name##_r1 asm(#r1) = v1;                                    \
     register t2 _##name##_r2 asm(#r2) = v2;                                    \
     asm volatile("jsr __" #name ":W"                                           \
-                 : "=r"(_rv), "=r"(_d1), "=r"(_a0), "=r"(_a1)                  \
+                 : "=r"(_d0), "=r"(_d1), "=r"(_a0), "=r"(_a1)                  \
                  : "r"(_##name##_r1), "r"(_##name##_r2)                        \
                  : "cc", "memory");                                            \
-    return _rv;                                                                \
+    return (rt)_d0;                                                            \
   }
 #else
 #define SYSCALL2(name, rt, t1, v1, r1, t2, v2, r2)                             \
@@ -114,7 +114,7 @@
 #ifndef _SYSTEM
 #define SYSCALL3(name, rt, t1, v1, r1, t2, v2, r2, t3, v3, r3)                 \
   static inline rt name(t1 v1, t2 v2, t3 v3) {                                 \
-    register rt _rv asm("d0");                                                 \
+    register int _d0 asm("d0");                                                \
     register int _d1 asm("d1");                                                \
     register int _a0 asm("a0");                                                \
     register int _a1 asm("a1");                                                \
@@ -122,10 +122,10 @@
     register t2 _##name##_r2 asm(#r2) = v2;                                    \
     register t3 _##name##_r3 asm(#r3) = v3;                                    \
     asm volatile("jsr __" #name ":W"                                           \
-                 : "=r"(_rv), "=r"(_d1), "=r"(_a0), "=r"(_a1)                  \
+                 : "=r"(_d0), "=r"(_d1), "=r"(_a0), "=r"(_a1)                  \
                  : "r"(_##name##_r1), "r"(_##name##_r2), "r"(_##name##_r3)     \
                  : "cc", "memory");                                            \
-    return _rv;                                                                \
+    return (rt)_d0;                                                            \
   }
 #else
 #define SYSCALL3(name, rt, t1, v1, r1, t2, v2, r2, t3, v3, r3)                 \
