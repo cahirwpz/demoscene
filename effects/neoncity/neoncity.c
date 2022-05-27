d70f0506407aa13ca8edfeea9b7fb0d8032a6521
Krystian Bacławski <krystian.baclawski@gmail.com>
2022-05-27 08:41:34 +0200

    Add missing function.

--- effects/neoncity/neoncity.c
+++ effects/neoncity/neoncity.c
@@ -1,10 +1,8 @@
 #include "effect.h"
-#include "hardware.h"
 #include "copper.h"
 #include "pixmap.h"
 #include "blitter.h"
 #include "gfx.h"
-#include "tasks.h"
 
 #define WIDTH 320
 #define HEIGHT 176
@@ -91,6 +89,32 @@ static __regargs void UpdateLayer2(int dstX, int srcX, short width) {
     tiles += layer2_map_width - width;
     dst_start += H * LINE_W / 8 - width * 2;
   } while (--h);
+}
+
+static void CopSetupDualPlayfield(CopListT *list, CopInsT **bplptr,
+                                  const BitmapT *pf1, const BitmapT *pf2)
+{
+  void **planes1 = pf1->planes;
+  void **planes2 = pf2->planes;
+  short n = pf1->depth + pf2->depth - 1;
+  short i = 0;
+
+  do {
+    void *plane = i & 1 ? *planes1++ : *planes2++;
+    CopInsT *ins = CopMove32(list, bplpt[i++], plane);
+
+    if (bplptr)
+      *bplptr++ = ins;
+  } while (--n != -1);
+
+
+  {
+    short bpl1mod = (short)pf1->bytesPerRow * (short)(pf1->depth - 1);
+    short bpl2mod = (short)pf2->bytesPerRow * (short)(pf2->depth - 1);
+
+    CopMove16(list, bpl1mod, pf1->flags & BM_INTERLEAVED ? bpl1mod : 0);
+    CopMove16(list, bpl2mod, pf2->flags & BM_INTERLEAVED ? bpl2mod : 0);
+  }
 }
 
 static void Init(void) {
