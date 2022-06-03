467b4df772b8565dce5ae056308274674320f041
MichalMyczkowski <myczkowskimichal@gmail.com>
2022-06-03 17:19:52 +0200

    encode.py got a bit uglier... But works with svgs exported from Inkscape!

--- effects/anim-polygons/encode.py
+++ effects/anim-polygons/encode.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import sys
 import os.path
 import argparse
 from textwrap import TextWrapper
@@ -8,21 +9,73 @@ import xml.etree.ElementTree as ET
 XMLNS = {"svg": "http://www.w3.org/2000/svg"}
 
 
-def get_coords(code):
-    xy = code.split(",")
-    x = int(xy[0])
-    y = int(xy[1])
-    return (x, y)
+def get_coords(letter, prev_coords, current_coords):
+    if letter == "M" or letter == "L":
+        return (current_coords[0], current_coords[1])
+    if letter == "m" or letter == "l":
+        x = prev_coords[0] + current_coords[0]
+        y = prev_coords[1] + current_coords[1]
+        return (x, y)
+    if letter == "H":
+        x = current_coords[0]
+        y = prev_coords[1]
+        return (x, y)
+    if letter == "h":
+        x = prev_coords[0] + current_coords[0]
+        y = prev_coords[1]
+        return (x, y)
+    if letter == "V":
+        x = prev_coords[0]
+        y = current_coords[0]
+        return (x, y)
+    if letter == "v":
+        x = prev_coords[0]
+        y = prev_coords[1] + current_coords[0]
+        return (x, y)
+    sys.exit(f"Unknown letter in path element: {letter}")
+
+
+def parse_numbers(frame_path, it):
+    coords = [0, 0]
+    i = it
+    j = it + 1
+    for xy in range(2):
+        while (frame_path[j].isdecimal() or frame_path[j] in "-.") \
+               and j < len(frame_path) - 1:
+            j += 1
+        coords[xy] = int(float(frame_path[i:j]))
+        while frame_path[j].isspace():
+            j += 1
+        if frame_path[j] != ",":
+            break
+        i = j + 1
+        j += 2
+    return (coords[0], coords[1]), j - 1
 
 
 def parse_frame(frame_path, polys):
     verts = []
-    for code in frame_path.split():
-        if code[0] == "Z":
-            polys.append(verts)
-            verts = []
-        else:
-            verts.append(get_coords(code[1:]))
+    frame_path += " "
+    letter = ""
+    current_coords = (0, 0)
+    prev_coords = (0, 0)
+
+    i = -1
+    while i < len(frame_path) - 1:
+        i += 1
+        if frame_path[i].isspace():
+            continue
+        if frame_path[i].isalpha():
+            letter = frame_path[i]
+            if letter.upper() == "Z":
+                polys.append(verts)
+                verts = []
+                prev_coords = (0, 0)
+            continue
+        if frame_path[i].isdecimal() or frame_path[i] == "-":
+            current_coords, i = parse_numbers(frame_path, i)
+            prev_coords = get_coords(letter, prev_coords, current_coords)
+            verts.append(prev_coords)
 
 
 def read_anim(path):
@@ -31,7 +84,12 @@ def read_anim(path):
     svg_element = tree.getroot()
     width = svg_element.attrib["width"]
     height = svg_element.attrib["height"]
+
     anim = svg_element[0]
+    for tag in svg_element:
+        if tag.attrib["id"] == "Animation":
+            anim = tag
+            break
 
     frames = []
     for frame in anim:
@@ -39,7 +97,6 @@ def read_anim(path):
         for element in frame[0]:
             parse_frame(element.attrib["d"], polys)
         frames.append(polys)
-
     return frames, int(width), int(height)
 
 
