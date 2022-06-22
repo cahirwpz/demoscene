3accc668bce0146d19375b324785b9e9b695c8e2
Krystian Bacławski <krystian.baclawski@gmail.com>
2022-06-22 22:34:22 +0200

    Write copper list address first, then enable copper.

--- lib/libgfx/CopListActivate.c
+++ lib/libgfx/CopListActivate.c
@@ -1,10 +1,10 @@
 #include <copper.h>
 
 void CopListActivate(CopListT *list) {
-  /* Enable copper DMA */
-  EnableDMA(DMAF_MASTER | DMAF_COPPER);
   /* Write copper list address. */
   custom->cop1lc = (u_int)list->entry;
+  /* Enable copper DMA */
+  EnableDMA(DMAF_MASTER | DMAF_COPPER);
   /* Wait for vertical blank to make sure the list is active. */
   WaitVBlank();
 }
