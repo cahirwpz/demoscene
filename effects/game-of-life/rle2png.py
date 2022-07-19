070588b45e0e66cb9b77d56a3142694cea481973
Kristopher38 <kristopher38@wp.pl>
2022-07-19 12:32:35 +0200

    Replace header regex with split and strip, reformat

--- effects/game-of-life/rle2png.py
+++ effects/game-of-life/rle2png.py
@@ -1,15 +1,11 @@
 #!/usr/bin/env python3
 
-from sys import argv
 from PIL import Image
 import re
 import argparse
 
 
-header_pattern = re.compile(
-    r'x\s*=\s*(\d+)\s*,\s*y\s*=\s*(\d+)(?:\s*,\s*rule\s*=\s*(\w+\/\w+)\s*)*'
-)
-rle_pattern = re.compile(r'(\d*)(o|b|\$)')
+rle_pattern = re.compile(r"(\d*)(o|b|\$)")
 rotate_transforms = {
     90: Image.ROTATE_90,
     180: Image.ROTATE_180,
@@ -18,21 +14,24 @@ rotate_transforms = {
 
 
 def convert_rle(infile):
-    with open(infile, 'r') as f:
+    with open(infile, "r") as f:
         # skip comments
         line = f.readline().strip()
         while line.startswith("#"):
             line = f.readline().strip()
 
-        if line == '':
+        if line == "":
             line = f.readline().strip()
-        header = line
+        header_line = line
 
         # read header
-        captures = header_pattern.match(header)
-        x = int(captures.group(1))
-        y = int(captures.group(2))
-        rule = captures.group(3)
+        header = {}
+        for property in header_line.split(","):
+            name, value = property.split("=")
+            header[name.strip()] = value.strip()
+
+        x = int(header["x"])
+        y = int(header["y"])
 
         # decode RLE into the image
         # pallete mode (only this can produce 1-bit image,
@@ -41,17 +40,19 @@ def convert_rle(infile):
         cur_x = 0
         cur_y = 0
         for line in f:
-            captures = [(int(count) if count != '' else 1, tag)
-                        for count, tag in rle_pattern.findall(line)]
+            captures = [
+                (int(count) if count != "" else 1, tag)
+                for count, tag in rle_pattern.findall(line)
+            ]
             for count, tag in captures:
                 for i in range(0, count):
-                    if tag == 'b':
+                    if tag == "b":
                         im.putpixel((cur_x, cur_y), 0)
                         cur_x += 1
-                    elif tag == 'o':
+                    elif tag == "o":
                         im.putpixel((cur_x, cur_y), 1)
                         cur_x += 1
-                    elif tag == '$':
+                    elif tag == "$":
                         cur_y += 1
                         cur_x = 0
                     else:
@@ -61,13 +62,18 @@ def convert_rle(infile):
         return im
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description='Converts RLE game of life description to 1-bit PNG.')
-    parser.add_argument('infile', help='Input RLE file')
-    parser.add_argument('outfile', help='Output PNG file')
-    parser.add_argument('--rotate', help='Rotate output image', type=int,
-                        choices=[90, 180, 270])
+        description="Converts RLE game of life description to 1-bit PNG."
+    )
+    parser.add_argument("infile", help="Input RLE file")
+    parser.add_argument("outfile", help="Output PNG file")
+    parser.add_argument(
+        "--rotate",
+        help="Rotate output image",
+        type=int,
+        choices=[90, 180, 270]
+    )
     args = parser.parse_args()
 
     im = convert_rle(args.infile)
