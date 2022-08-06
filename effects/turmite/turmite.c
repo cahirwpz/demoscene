8ba0b4b7622fcf0e7d8032379815633d1e9b2046
Krystian Bacławski <krystian.baclawski@gmail.com>
2022-08-06 14:52:13 +0200

    Two drawing methods: additive and generation based.

--- effects/turmite/turmite.c
+++ effects/turmite/turmite.c
@@ -11,12 +11,15 @@
 
 #define WIDTH 256
 #define HEIGHT 256
-#define DEPTH 1
+#define DEPTH 4
 #define NSTEPS 256
+
+#define GENERATION 1
 
 static CopListT *cp;
 static BitmapT *screen;
 static u_char board[WIDTH * HEIGHT];
+static u_char value[WIDTH * HEIGHT];
 
 static void Init(void) {
   screen = NewBitmap(WIDTH, HEIGHT, DEPTH);
@@ -25,8 +28,6 @@ static void Init(void) {
   SetupBitplaneFetch(MODE_LORES, X(32), WIDTH);
   SetupMode(MODE_LORES, DEPTH);
   LoadPalette(&turmite_pal, 0);
-
-  custom->color[1] = turmite_pal.colors[8];
 
   cp = NewCopList(100);
   CopInit(cp);
@@ -59,7 +60,7 @@ typedef struct Turmite {
   u_short pos;
   short state;
   short dir;
-  RuleT rules[2][2];
+  RuleT rules[3][2];
 } TurmiteT;
 
 /* board color is assumed to be binary */
@@ -97,12 +98,86 @@ TurmiteT ChaoticGrowth = {
   }
 };
 
-static const u_short PosChange[4] = { +WIDTH, -1, -WIDTH, +1 };
+TurmiteT SnowFlake = {
+  .pos = POS(128, 128),
+  .dir = 0,
+  .state = 0,
+  .rules = {
+    {
+      RULE(1, -1, 1),
+      RULE(1, +1, 0),
+    }, {
+      RULE(1, +2, 1),
+      RULE(1, +2, 2),
+    }, {
+      RULE(0, +2, 0),
+      RULE(0, +2, 0),
+    }
+  }
+};
 
-static inline void FlipPixel(u_char *bpl, u_int pos) {
-  short offset = pos >> 3;
+TurmiteT Irregular = {
+  .pos = POS(128, 128),
+  .dir = 0,
+  .state = 0,
+  .rules = {
+    {
+      RULE(1, +1, 0),
+      RULE(1, +1, 1),
+    }, {
+      RULE(0, 0, 0),
+      RULE(0, 0, 1),
+    }
+  }
+};
+
+static const u_short PosChange[4] = {
+  [SOUTH] = +WIDTH,
+  [WEST] = -1,
+  [NORTH] = -WIDTH,
+  [EAST] = +1,
+};
+
+#define BPLSIZE (WIDTH * HEIGHT / 8)
+
+static inline void BitValue(u_char *bpl, short offset, u_char bit, u_char val) {
+  if (val)
+    bset(bpl + offset, bit);
+  else
+    bclr(bpl + offset, bit);
+}
+
+static inline void PixelValue(u_char *bpl, u_char bit, u_char val) {
+  BitValue(bpl, BPLSIZE * 0, bit, val & 1);
+  BitValue(bpl, BPLSIZE * 1, bit, val & 2);
+  BitValue(bpl, BPLSIZE * 2, bit, val & 4);
+  BitValue(bpl, BPLSIZE * 3, bit, val & 8);
+}
+
+static inline void SetPixel(u_char *bpl, u_short pos, u_char val) {
+  int offset = pos >> 3;
   u_char bit = ~pos;
-  bchg(&bpl[offset], bit);
+
+  bpl += offset;
+  
+  switch (val >> 4) {
+    case 0: PixelValue(bpl, bit, 0); break;
+    case 1: PixelValue(bpl, bit, 1); break;
+    case 2: PixelValue(bpl, bit, 2); break;
+    case 3: PixelValue(bpl, bit, 3); break;
+    case 4: PixelValue(bpl, bit, 4); break;
+    case 5: PixelValue(bpl, bit, 5); break;
+    case 6: PixelValue(bpl, bit, 6); break;
+    case 7: PixelValue(bpl, bit, 7); break;
+    case 8: PixelValue(bpl, bit, 8); break;
+    case 9: PixelValue(bpl, bit, 9); break;
+    case 10: PixelValue(bpl, bit, 10); break;
+    case 11: PixelValue(bpl, bit, 11); break;
+    case 12: PixelValue(bpl, bit, 12); break;
+    case 13: PixelValue(bpl, bit, 13); break;
+    case 14: PixelValue(bpl, bit, 14); break;
+    case 15: PixelValue(bpl, bit, 15); break;
+  }
 }
 
 static inline RuleT *GetRule(TurmiteT *t, short col) {
@@ -111,22 +186,46 @@ static inline RuleT *GetRule(TurmiteT *t, short col) {
   return rule + offset;
 }
 
-#if 0
+#if GENERATION
+static u_char generation = 0;
+#endif
+
+static TurmiteT *TheTurmite =
+#if GENERATION
+&SnowFlake;
+#else
+&Irregular;
+#endif
+
+#if 1
 static inline
 #endif
-void TurmiteMove(TurmiteT *t, u_char *board, u_char *bpl) {
+void TurmiteMove(TurmiteT *t, u_char *board, u_char *value, u_char *bpl) {
   int pos = t->pos;
   short col = board[pos];
   RuleT *rule = GetRule(t, col);
+
+  (void)value;
 
   t->state = rule->nstate;
 
   {
     short newcol = rule->ncolor;
+    u_char val;
+
     board[pos] = newcol;
 
-    if (newcol != col)
-      FlipPixel(bpl, pos);
+#if GENERATION
+    val = generation;
+#else
+    val = value[pos];
+    val += 4;
+    if (val == 0)
+       val -= 4;
+    value[pos] = val;
+#endif
+
+    SetPixel(bpl, pos, val);
   }
 
   {
@@ -137,13 +236,17 @@ void TurmiteMove(TurmiteT *t, u_char *board, u_char *bpl) {
   }
 }
 
-/* static */ void SimulateTurmite(void) {
+void SimulateTurmite(void) {
   u_char *bpl = screen->planes[0]; 
   short i;
 
   for (i = 0; i < NSTEPS; i++) {
-    TurmiteMove(&ChaoticGrowth, board, bpl);
+    TurmiteMove(TheTurmite, board, value, bpl);
   }
+
+#if GENERATION
+  generation += 2;
+#endif
 }
 
 PROFILE(SimulateTurmite);
