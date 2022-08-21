a9f797034374cc4da9f971d3bfff5a8dce2ee4dd
Krystian Bacławski <krystian.baclawski@gmail.com>
2022-08-21 23:28:20 +0200

    First attempt to add fruits.

--- effects/growing-tree/growing-tree.c
+++ effects/growing-tree/growing-tree.c
@@ -15,6 +15,8 @@ static CopListT *cp;
 static CopInsT *bplptr[DEPTH];
 static BitmapT *screen;
 
+#include "data/fruit.c"
+
 typedef struct Branch {
   short pos_x, pos_y; // Q12.4
   union { // Q4.12
@@ -90,6 +92,111 @@ static inline BranchT *NewBranch(void) {
   }
 }
 
+#define screen_bytesPerRow (WIDTH / 8)
+
+static void CopyFruit(u_short x, u_short y) {
+  u_short dstmod = screen_bytesPerRow - fruit_bytesPerRow;
+  u_short bltshift = rorw(x & 15, 4);
+  u_short bltsize = (fruit_height << 6) | (fruit_bytesPerRow >> 1);
+  void *srcbpt = fruit.planes[0];
+  void *dstbpt = screen->planes[1];
+  u_short bltcon0;
+
+  if (bltshift)
+    bltsize++, dstmod -= 2;
+
+  dstbpt += (x & ~15) >> 3;
+  dstbpt += y * screen_bytesPerRow;
+  bltcon0 = (SRCA | SRCB | DEST | A_OR_B) | bltshift;
+
+  WaitBlitter();
+
+  if (bltshift) {
+    custom->bltalwm = 0;
+    custom->bltamod = -2;
+  } else {
+    custom->bltalwm = -1;
+    custom->bltamod = 0;
+  }
+
+  custom->bltbmod = dstmod;
+  custom->bltdmod = dstmod;
+  custom->bltcon0 = bltcon0;
+  custom->bltcon1 = 0;
+  custom->bltafwm = -1;
+  custom->bltapt = srcbpt;
+  custom->bltbpt = dstbpt;
+  custom->bltdpt = dstbpt;
+  custom->bltsize = bltsize;
+}
+
+static void DrawBranch(short x1, short y1, short x2, short y2) {
+  u_char *data = screen->planes[0];
+  short dx, dy, derr;
+
+  u_short bltcon0 = BC0F_LINE_OR;
+  u_short bltcon1 = LINEMODE;
+
+  /* Always draw the line downwards. */
+  if (y1 > y2) {
+    swapr(x1, x2);
+    swapr(y1, y2);
+  }
+
+  bltcon0 |= rorw(x1 & 15, 4);
+
+  /* Word containing the first pixel of the line. */
+  data += screen_bytesPerRow * y1;
+  data += (x1 >> 3) & ~1;
+
+  dx = x2 - x1;
+  dy = y2 - y1;
+
+  if (dx < 0) {
+    dx = -dx;
+    if (dx >= dy) {
+      bltcon1 |= AUL | SUD;
+    } else {
+      bltcon1 |= SUL;
+      swapr(dx, dy);
+    }
+  } else {
+    if (dx >= dy) {
+      bltcon1 |= SUD;
+    } else {
+      swapr(dx, dy);
+    }
+  }
+
+  derr = dy + dy - dx;
+  if (derr < 0)
+    bltcon1 |= SIGNFLAG;
+
+  {
+    u_short bltamod = derr - dx;
+    u_short bltbmod = dy + dy;
+    u_short bltsize = (dx << 6) + 66;
+
+    WaitBlitter();
+
+    custom->bltafwm = -1;
+    custom->bltalwm = -1;
+    custom->bltadat = 0x8000;
+    custom->bltbdat = -1;
+    custom->bltcmod = screen_bytesPerRow;
+    custom->bltdmod = screen_bytesPerRow;
+
+    custom->bltcon0 = bltcon0;
+    custom->bltcon1 = bltcon1;
+    custom->bltamod = bltamod;
+    custom->bltbmod = bltbmod;
+    custom->bltapt = (void *)(int)derr;
+    custom->bltcpt = data;
+    custom->bltdpt = data;
+    custom->bltsize = bltsize;
+  }
+}
+
 static void MakeBranch(short x, short y) {
   BranchT *b = NewBranch();
   if (!b)
@@ -107,22 +214,24 @@ static inline void KillBranch(BranchT *b, BranchT **lastp) {
     *b = *last;
 }
 
-static void SplitBranch(BranchT *parent, BranchT **lastp) {
+static bool SplitBranch(BranchT *parent, BranchT **lastp) {
   short newDiameter = normfx(mul16(parent->diameter, fx12f(0.58)));
 
-  if (newDiameter < fx4f(0.2)) {
-    KillBranch(parent, lastp);
-  } else {
+  if (newDiameter >= fx4f(0.2)) {
     BranchT *b = NewBranch();
     if (!b)
-      return;
+      return true;
     b->pos_x = parent->pos_x;
     b->pos_y = parent->pos_y;
     b->vel_x = parent->vel_x;
     b->vel_y = parent->vel_y;
     b->diameter = newDiameter;
     parent->diameter = newDiameter;
+    return true;
   }
+
+  KillBranch(parent, lastp);
+  return false;
 }
 
 static inline int shift14(short a) {
@@ -175,13 +284,25 @@ void GrowingTree(BranchT *branches, BranchT **lastp) {
     {
       KillBranch(b, lastp);
     } else {
-      BlitterLine(prev_x >> 4, prev_y >> 4, curr_x >> 4, curr_y >> 4);
-
       b->pos_x = curr_x;
       b->pos_y = curr_y;
 
+      prev_x >>= 4;
+      prev_y >>= 4;
+      curr_x >>= 4;
+      curr_y >>= 4;
+      
+      DrawBranch(prev_x, prev_y, curr_x, curr_y);
+
       if ((u_short)random() < (u_short)(65536 * 0.2f)) {
-        SplitBranch(b, lastp);
+        if (!SplitBranch(b, lastp) && b->diameter < fx4f(0.3)) {
+          curr_x -= fruit_width / 8;
+          curr_y -= fruit_height / 8;
+
+          if ((curr_x >= 0) && (curr_x < WIDTH - fruit_width) &&
+              (curr_y >= 0) && (curr_y < HEIGHT - fruit_height))
+            CopyFruit(curr_x, curr_y);
+        }
       }
     }
   }
@@ -190,8 +311,6 @@ void GrowingTree(BranchT *branches, BranchT **lastp) {
 PROFILE(GrowTree);
 
 static void Render(void) {
-  BlitterLineSetup(screen, 0, LINE_OR|LINE_SOLID);
-
   if (lastBranch == branches) {
     BitmapClear(screen);
     MakeBranch(WIDTH / 2, HEIGHT - 1);
