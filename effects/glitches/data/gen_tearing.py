ddb5015d54c70d6721e16b5a4b69e618863c0643
Krystian Bacławski <krystian.baclawski@gmail.com>
2024-08-25 13:08:20 +0200

    fix python style

--- effects/glitches/data/gen_tearing.py
+++ effects/glitches/data/gen_tearing.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python3
+
 from contextlib import redirect_stdout
 import sys
 import random
 
 arr = [[0] * 16 for i in range(16)]
 
+
 def generateRandomTable():
     o = random.randint(5, 16)
     u = random.randint(0, 2)
 
-    print (o)
+    print(o)
     bby = [[0] * o for i in range(o)]
 
     for i in range(o):
@@ -24,6 +26,7 @@ def generateRandomTable():
         bby[i] = b
     return bby
 
+
 def generateTable():
     arr = [[0] * 16 for i in range(16)]
 
@@ -32,7 +35,7 @@ def generateTable():
             if j < i:
                 value = i - j + random.randint(-5, 5)
                 if value <= 0:
-                    value = 0;
+                    value = 0
                     arr[i][15-j] = value
                 else:
                     arr[i][15-j] = hex(value*17)
@@ -43,20 +46,21 @@ def generateTable():
         arr[i] = b
     return arr
 
+
 def saveFile(bby):
     with open(sys.argv[1], 'w') as f:
         with redirect_stdout(f):
             print("static __data u_short tears[16][32] = {")
             for aa in bby:
-                print ("    {", end=" ")
+                print("    {", end=" ")
                 for a in aa:
                     if a == 0:
                         print("0x00,", end=" ")
                     else:
                         print(f"{a},", end=" ")
-                print ("},")
+                print("},")
             print('};\n')
+
 
 bby = generateTable()
 saveFile(bby)
-
