923fd42512a4d698b311dd684ef842697c3de0bc
polprog <gorplop@sdf.org>
2025-04-23 20:34:04 +0200

    roller: prototype support for VPOS=0xff crossing

--- effects/roller/roller.c
+++ effects/roller/roller.c
@@ -14,9 +14,14 @@
 
 static __code CopListT *cp;
 
-// For now, only 32 transitions (2 rows of texture)
+
+// 16 transitions = 1 row of texture
 #define NTRANSITIONS (5*16)
-static CopInsT *ciTransition[NTRANSITIONS];
+#define VPSTART (0xc8 - 32)
+#define ROWHEIGHT 2
+#define __ANIMATE 0
+
+static CopInsT *ciTransition[NTRANSITIONS+1];
 
 #include "data/roller-bg.c"
 #include "data/magland16.c"
@@ -27,26 +32,42 @@ typedef u_short palentry_t;
 static CopListT *MakeCopperList(CopListT *cp) {
   palentry_t *p = texture_pal_colors;
   short i, j;
+  short vp = VPSTART;
+  short ffcross = 0;
+  (void) i;
   CopSetupBitplanes(cp, &roller_bp, S_DEPTH);
   CopSetColor(cp, 0, 0x000);
-  
-  
-  for(i = 0; i < NTRANSITIONS; i++){
-    // cp->curr now points to the CopWait that's about to happen
-    ciTransition[i] = cp->curr;
-    CopWait(cp, 0x55, 0); // vpos is overwritten in Render
+#if 1
+  // Test - all green before vp = 0xff
+  for(j = 1; j < 16; j++){
+      CopSetColor(cp, j, 0x0F0);
+      (void) p;
+  }
+#endif
+ 
+  CopSetColor(cp, 0, 0xFFF);
 
-    // VPOS=255 is in the middle of the roller, so we have to insert this
-    // and then take care to sync it when rolling
-    if(i == -1)
-      CopWait(cp, 255, 0);
-    
+  for(i = 0; i < NTRANSITIONS+1; i++){
+    // cp->curr now points to the CopWait that's about to happen
+   
+    vp += ROWHEIGHT;
+    if(!ffcross && vp > 0xFF){
+      ffcross = 1;
+      ciTransition[i] = cp->curr;
+      CopWait(cp, 0xFF, 0xDF); // Wait for VPOS overflow
+      CopSetColor(cp, 0, 0x222); // Debug, show copperlist position on raster
+      Log("inserted copper safe wait at %d (%p)\n", i, ciTransition[i]);
+      i++;
+    }
+    ciTransition[i] = cp->curr;
+    CopWait(cp, vp, 0x00); // vpos, hpos is overwritten in Render
     // XXX: change texture to cmap4.
     for(j = 1; j < 16; j++){
       CopSetColor(cp, j, p[texture_bp_pixels[(i*16 + j) % (256)]]);
+      
     }
   }
-  
+
   return CopListFinish(cp);
 }
 
@@ -105,17 +126,33 @@ static void Render(void) {
 
   static short framen = 0;
   short i = 0;
-  
   // Patch the coppper instructions in memory
-  for(i = 0; i < NTRANSITIONS; i++) {
-    //ciTransition[i]->wait.vp = 0x80 + (framen>>1) + 4*i;
-    
-    ciTransition[i]->wait.vp = (0x50 + (framen>>1) + 2*i);
-  }
+  short vpos = VPSTART + (framen>>2);
+  short ffcross = 0;
 
-  framen++;
-  framen &= 0x7F;
+  (void) ffcross; (void) vpos; (void) i;
+#if __ANIMATE
+  for(i = 0; i < NTRANSITIONS+1; ++i) {
+
+    vpos +=  ROWHEIGHT;
+    // Calculate where the crossing now occurs
+    if(!ffcross && vpos > 0xff){     
+      ffcross = 1;
+      // Insert safe wait
+      ciTransition[i]->wait.vp = 0xFF;
+      ciTransition[i]->wait.hp = 0xDF;
+      Log("VPOS overflow at wait number %d\n", i);
+      i++;
+    }
+    
+    ciTransition[i]->wait.vp = vpos & 0xFF;
+    ciTransition[i]->wait.hp = 0x81;
   
+  }
+#endif
+  framen++;
+  framen &= 0x3F;
+  (void) ciTransition;
   TaskWaitVBlank();
 }
 
