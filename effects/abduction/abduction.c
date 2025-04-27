2958c3e39c0fdcdb50aa77574bd6e1b650e9cffc
Krystian Bacławski <krystian.baclawski@gmail.com>
2025-04-27 15:29:27 +0200

    Small fix.

--- effects/abduction/abduction.c
+++ effects/abduction/abduction.c
@@ -179,9 +179,9 @@ static void RetractBeam(void) {
     }
 
     if (beam_pos[0].hpos >= 137 + 15 + DIWHP) {
-      SpriteUpdatePos(&side_beam_l, X(0), Y(0));
-      SpriteUpdatePos(&side_beam_r, X(0), Y(0));
-      SpriteUpdatePos(&coq, X(0), Y(0));
+      SpriteUpdatePos(&side_beam_l, HP(0), VP(0));
+      SpriteUpdatePos(&side_beam_r, HP(0), VP(0));
+      SpriteUpdatePos(&coq, HP(0), VP(0));
       phase = ESCAPE;
     }
   }
